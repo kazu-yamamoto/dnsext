4b466aa71413624cae245f5e54d463f94711d8e7
Kei Hibino <ex8k.hibino@gmail.com>
2022-03-09 14:26:57 +0900

    iterative: print query-error for tests.

--- qtest/QuerySpec.hs
+++ qtest/QuerySpec.hs
@@ -11,52 +11,61 @@ import DNSC.Iterative (runQuery, runQuery1, runIterative, rootNS)
 spec :: Spec
 spec = describe "query" $ do
 
+  let printQueryError :: Show e => Either e a -> IO ()
+      printQueryError = either (putStrLn . ("    QueryError: " ++) . show) (const $ pure ())
+
   it "rootNS" $ do
     let sp p = case p of (_,_) -> True  -- check not error
     rootNS `shouldSatisfy` sp
 
   it "iterative" $ do
-    let domain = "iij.ad.jp."
-    result <- runIterative rootNS domain
+    result <- runIterative rootNS "iij.ad.jp."
+    printQueryError result
     result `shouldSatisfy` isRight
 
   it "iterative - long" $ do
-    let domain = "c.b.a.pt.dns-oarc.net."
-    result <- runIterative rootNS domain
+    result <- runIterative rootNS "c.b.a.pt.dns-oarc.net."
+    printQueryError result
     result `shouldSatisfy` isRight
 
   it "query1 - ns" $ do
     result <- runQuery1 "iij.ad.jp." NS
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
 
   it "query1 - a" $ do
     result <- runQuery1 "iij.ad.jp." A
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
 
   it "query1 - aaaa" $ do
     result <- runQuery1 "iij.ad.jp." AAAA
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
 
   it "query1 - mx" $ do
     result <- runQuery1 "iij.ad.jp." MX
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
 
   it "query1 - cname" $ do
     result <- runQuery1 "porttest.dns-oarc.net." CNAME
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
 
   it "query - a" $ do
     result <- runQuery "porttest.dns-oarc.net." TXT
+    printQueryError result
     isRight result `shouldBe` True
     let Right msg = result
     length (DNS.answer msg) `shouldSatisfy` (> 0)
