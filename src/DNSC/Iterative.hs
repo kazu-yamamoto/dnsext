be83be0fd2931a682a5c332fcbb88fd90cc15155
Kei Hibino <ex8k.hibino@gmail.com>
2022-03-30 18:55:57 +0900

    iterative: drop redundant import.

--- src/DNSC/Iterative.hs
+++ src/DNSC/Iterative.hs
@@ -19,7 +19,6 @@ import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (ExceptT (..), runExceptT, throwE)
 import Control.Monad.Trans.Reader (ReaderT (..), asks)
 import qualified Data.ByteString.Char8 as B8
-import Data.Ord (comparing)
 import Data.Maybe (mapMaybe, listToMaybe)
 import Data.List (isSuffixOf, unfoldr, intercalate, uncons, sort, sortOn)
 import Data.Bits ((.&.), shiftR)
