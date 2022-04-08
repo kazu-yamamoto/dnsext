d41f41f9aca5036b6615697b8930d419e0419e15
Kei Hibino <ex8k.hibino@gmail.com>
2022-04-08 12:36:01 +0900

    cache-server: server implementation.

---
+++ src/DNSC/Server.hs
@@ -0,0 +1,132 @@
+module DNSC.Server (
+  run,
+
+  bind, monitor,
+  ) where
+
+import Control.Monad (forever, (<=<), unless)
+import Control.Concurrent (forkIO)
+import Data.Ord (Down (..))
+import Data.List (uncons, isInfixOf)
+import qualified Data.ByteString.Char8 as B8
+
+import Network.Socket (AddrInfo (..), SocketType (Datagram), HostName, PortNumber, Socket, SockAddr)
+import qualified Network.Socket as S
+import Network.DNS (DNSMessage, DNSHeader, Question)
+import qualified Network.DNS as DNS
+
+import DNSC.Concurrent (forkConsumeQueue, forksConsumeQueue)
+import DNSC.DNSUtil (recvFrom, sendTo)
+import qualified DNSC.Log as Log
+import DNSC.Iterative (Context (..), newContext, runReply)
+
+
+type NE a = (a, [a])
+
+type Request s a = (s, (DNSHeader, NE Question), a)
+type Response s a = ((s, DNSMessage), a)
+
+udpSockets :: PortNumber -> [HostName] -> IO [(Socket, SockAddr)]
+udpSockets port = mapM aiSocket . filter ((== Datagram) . addrSocketType) <=< addrInfo port
+  where
+    aiSocket ai = (,) <$> S.socket (addrFamily ai) (addrSocketType ai) (addrProtocol ai) <*> pure (addrAddress ai)
+
+addrInfo :: PortNumber -> [HostName] -> IO [AddrInfo]
+addrInfo p []        = S.getAddrInfo Nothing Nothing $ Just $ show p
+addrInfo p hs@(_:_)  = concat <$> sequence [ S.getAddrInfo Nothing (Just h) $ Just $ show p | h <- hs ]
+
+run :: Log.Level -> Bool -> Int
+    -> PortNumber -> [HostName] -> IO ()
+run level disableV6NS conc port hosts =
+  uncurry monitor =<< bind level disableV6NS conc port hosts
+
+bind :: Log.Level -> Bool -> Int
+     -> PortNumber -> [HostName]
+     -> IO (Context, IO ())
+bind level disableV6NS para port hosts = do
+  (putLines, quitLog) <- Log.new level
+  (cxt, quitCache) <- newContext putLines disableV6NS
+
+  sas <- udpSockets port hosts
+
+  (enqueueResp, quitResp) <- forkConsumeQueue (sendResponse sendTo cxt)
+  (enqueueReq, quitProc)  <- forksConsumeQueue para $ processRequest cxt enqueueResp
+  sequence_ [ forkIO $ forever $ recvRequest recvFrom cxt enqueueReq sock | (sock, _) <- sas ]
+
+  mapM_ (uncurry S.bind) sas
+
+  let quit = do
+        quitProc
+        quitResp
+        quitCache
+        quitLog
+
+  return (cxt, quit)
+
+recvRequest :: Show a
+            => (s -> IO (DNSMessage, a))
+            -> Context
+            -> (Request s a -> IO ())
+            -> s
+            -> IO ()
+recvRequest recv cxt enqReq sock =  do
+ (m, addr) <- recv sock
+ let logLn level = logLines_ cxt level . (:[])
+     enqueue qs = enqReq (sock, (DNS.header m, qs), addr)
+     emptyWarn = logLn Log.NOTICE $ "empty question ignored: " ++ show addr
+ maybe emptyWarn enqueue $ uncons $ DNS.question m
+
+processRequest :: Show a
+               => Context
+               -> (Response s a -> IO ())
+               -> Request s a -> IO ()
+processRequest cxt enqResp (sock, rp@(_, (q,_)), addr) = do
+  let enqueue m = enqResp ((sock, m), addr)
+      logLn level = logLines_ cxt level . (:[])
+      noResponse replyErr = logLn Log.NOTICE $ "response cannot be generated: " ++ replyErr ++ ": " ++ show (q, addr)
+  either noResponse enqueue =<< uncurry (runReply cxt) rp
+
+sendResponse :: (s -> DNSMessage -> a -> IO ())
+             -> Context
+             -> Response s a -> IO ()
+sendResponse send _cxt = uncurry (uncurry send)
+
+data Command
+  = Find String
+  | Lookup DNS.Domain DNS.TYPE
+  | Size
+  | Noop
+  | Quit
+  deriving Show
+
+monitor :: Context -> IO () -> IO ()
+monitor cxt quit = loop
+  where
+    parseTYPE "A"      = Just DNS.A
+    parseTYPE "AAAA"   = Just DNS.AAAA
+    parseTYPE "NS"     = Just DNS.NS
+    parseTYPE "CNAME"  = Just DNS.CNAME
+    parseTYPE _        = Nothing
+    parseCmd []  =    Just Noop
+    parseCmd ws  =  case ws of
+      "find" : s : _           ->  Just $ Find s
+      "lookup" : n : typ : []  ->  Lookup (B8.pack n) <$> parseTYPE typ
+      "size" : _  ->  Just Size
+      "quit" : _  ->  Just Quit
+      _           ->  Nothing
+
+    runCmd Quit  =  quit *> return True
+    runCmd cmd   =  dispatch cmd *> return False
+      where
+        dispatch Noop             =  return ()
+        dispatch (Find s)         =  mapM_ putStrLn . filter (s `isInfixOf`) . map show =<< dump_ cxt
+        dispatch (Lookup dom typ) =  maybe (putStrLn "miss.") hit =<< lookup_ cxt dom typ DNS.classIN
+          where hit (rrs, Down rank) = mapM_ putStrLn $ ("hit: " ++ show rank) : map show rrs
+        dispatch Size             = (print =<< size_ cxt)
+        dispatch x                =  putStrLn $ "command: unknown state: " ++ show x
+
+    loop = do
+      putStr "\nmonitor:\n"
+      s <- getLine
+      isQuit <- maybe (putStrLn "command: parse error" *> return False) runCmd $ parseCmd $ words s
+      unless isQuit loop
