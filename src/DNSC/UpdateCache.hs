9318e087f185e6c4fa84d192fca11a76a4be073f
Kei Hibino <ex8k.hibino@gmail.com>
2022-04-12 22:21:29 +0900

    quit-forked-threads: apply quit-interface to UpdateCache.

--- src/DNSC/UpdateCache.hs
+++ src/DNSC/UpdateCache.hs
@@ -2,13 +2,12 @@ module DNSC.UpdateCache (
   newCache,
   ) where
 
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Concurrent.Chan (newChan, readChan, writeChan)
-import Control.Monad (void, forever)
+import Control.Concurrent (threadDelay)
 import Data.IORef (newIORef, readIORef, writeIORef)
 
 import Network.DNS (TTL, Domain, TYPE, CLASS, ResourceRecord)
 
+import DNSC.Concurrent
 import qualified DNSC.Log as Log
 import DNSC.Cache (Cache, Key, CRSet, Ranking, Timestamp, getTimestamp)
 import qualified DNSC.Cache as Cache
@@ -26,14 +25,12 @@ runUpdate t u = case u of
 type Lookup = Domain -> TYPE -> CLASS -> IO (Maybe ([ResourceRecord], Ranking))
 type Insert = Key -> TTL -> CRSet -> Ranking -> IO ()
 
-newCache :: (Log.Level -> [String] -> IO ()) -> IO (Lookup, Insert, IO Cache)
+newCache :: (Log.Level -> [String] -> IO ()) -> IO (Lookup, Insert, IO Cache, IO ())
 newCache putLines = do
   let putLn level = putLines level . (:[])
   cacheRef <- newIORef Cache.empty
-  updateQ <- newChan
 
-  let update1 = do   -- step of single update theard
-        (ts, u) <- readChan updateQ
+  let update1 (ts, u) = do   -- step of single update theard
         cache <- readIORef cacheRef
         let updateRef c = do
               writeIORef cacheRef c
@@ -41,12 +38,12 @@ newCache putLines = do
                 I {}  ->  return ()
                 E     ->  putLn Log.NOTICE $ show ts ++ ": some records expired: size = " ++ show (Cache.size c)
         maybe (pure ()) updateRef $ runUpdate ts u cache
-  void $ forkIO $ forever update1
+  (enqueueU, quitU) <- forkProcessQ update1
 
   let expires1 = do
         threadDelay $ 1000 * 1000
-        writeChan updateQ =<< (,) <$> getTimestamp <*> pure E
-  void $ forkIO $ forever expires1
+        enqueueU =<< (,) <$> getTimestamp <*> pure E
+  quitE <- forkLoop expires1
 
   let lookup_ dom typ cls = do
         cache <- readIORef cacheRef
@@ -54,6 +51,6 @@ newCache putLines = do
         return $ Cache.lookup ts dom typ cls cache
 
       insert k ttl crs rank =
-        writeChan updateQ =<< (,) <$> getTimestamp <*> pure (I k ttl crs rank)
+        enqueueU =<< (,) <$> getTimestamp <*> pure (I k ttl crs rank)
 
-  return (lookup_, insert, readIORef cacheRef)
+  return (lookup_, insert, readIORef cacheRef, quitE *> quitU)
