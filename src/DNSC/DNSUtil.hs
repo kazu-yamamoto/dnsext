94a521267d4a345243c36eb0f7afdeefff09d3ff
Kei Hibino <ex8k.hibino@gmail.com>
2022-04-19 22:12:11 +0900

    socket-recv-send-msg: add interface to apply recvMsg and sendMsg when available.

--- src/DNSC/DNSUtil.hs
+++ src/DNSC/DNSUtil.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE CPP #-}
 
 module DNSC.DNSUtil (
+  mkRecv, mkSend,
   recvFrom, sendTo
   ) where
 
@@ -9,12 +11,67 @@ import Control.Monad (void)
 import Time.System (timeCurrent)
 import qualified Time.Types as Time
 import Network.Socket (Socket, SockAddr)
+#if MIN_VERSION_network(3,1,2)
+import qualified Network.Socket as Socket
+#endif
 import qualified Network.Socket.ByteString as Socket
 import Network.DNS (DNSMessage)
 import qualified Network.DNS as DNS
 
 
 ---
+
+#if MIN_VERSION_network(3,1,2)
+type Cmsg = Socket.Cmsg
+#else
+type Cmsg = ()
+#endif
+
+mkRecv :: Bool -> Socket -> IO (DNSMessage, (SockAddr, [Cmsg]))
+#if MIN_VERSION_network(3,1,2)
+mkRecv wildcard
+  | wildcard    =  recvDNS recvMsg
+  | otherwise   =  recvDNS recvFrom
+#else
+mkRecv _        =  recvDNS recvFrom
+#endif
+  where
+    recvDNS recv sock = do
+      (bs, ai) <- recv sock `E.catch` \e -> E.throwIO $ DNS.NetworkFailure e
+      Time.Elapsed (Time.Seconds now) <- timeCurrent
+      case DNS.decodeAt now bs of
+        Left  e   -> E.throwIO e
+        Right msg -> return (msg, ai)
+
+#if MIN_VERSION_network(3,1,2)
+    recvMsg sock = do
+      let cbufsiz = 64
+      (peer, bs, cmsgs, _) <- Socket.recvMsg sock bufsiz cbufsiz 0
+      return (bs, (peer, cmsgs))
+#endif
+
+    recvFrom sock = do
+      (bs, peer) <- Socket.recvFrom sock bufsiz
+      return (bs, (peer, []))
+    bufsiz = 16384 -- maxUdpSize in dns package, internal/Network/DNS/Types/Internal.hs
+
+mkSend :: Bool -> Socket -> DNSMessage -> (SockAddr, [Cmsg]) -> IO ()
+#if MIN_VERSION_network(3,1,2)
+mkSend wildcard
+  | wildcard   =  sendDNS sendMsg
+  | otherwise  =  sendDNS sendTo
+#else
+mkSend _       =  sendDNS sendTo
+#endif
+  where
+    sendDNS send sock msg (addr, cmsgs) =
+      void $ send sock (DNS.encode msg) addr cmsgs
+
+#if MIN_VERSION_network(3,1,2)
+    sendMsg sock bs addr cmsgs = Socket.sendMsg sock addr [bs] cmsgs 0
+#endif
+
+    sendTo sock bs addr _ = Socket.sendTo sock bs addr
 
 -- receive DNSMessage from client with address
 recvFrom :: Socket -> IO (DNSMessage, SockAddr)
