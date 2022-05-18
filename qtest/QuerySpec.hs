0fb9f598699feda1340725a77a0e1f2e0359e527
Kei Hibino <ex8k.hibino@gmail.com>
2022-05-18 19:07:11 +0900

    read-queue-size: qtest: along with updated interface.

--- qtest/QuerySpec.hs
+++ qtest/QuerySpec.hs
@@ -18,7 +18,7 @@ spec :: Spec
 spec = describe "query" $ do
   disableV6NS <- runIO $ maybe False ((== "1") . take 1) <$> lookupEnv "DISABLE_V6_NS"
   (tcache, _quitT) <- runIO TimeCache.new
-  (ucache, _quit) <- runIO $ UCache.new (\_ _ -> pure ()) tcache $ 2 * 1024 * 1024
+  (ucache, _, _quit) <- runIO $ UCache.new (\_ _ -> pure ()) tcache $ 2 * 1024 * 1024
   cxt <- runIO $ newContext (\_ _ -> pure ()) disableV6NS ucache tcache
   cxt4 <- runIO $ newContext (\_ _ -> pure ()) True ucache tcache
   let runIterative ns n = runDNSQuery (iterative ns n) cxt
