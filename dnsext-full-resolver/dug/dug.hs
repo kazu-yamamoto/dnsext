5f47d8e11e2bf422be3945b5f89731757c52d614
Kazu Yamamoto <kazu@iij.ad.jp>
2022-10-18 12:50:18 +0900

    hlint suggestions

--- dnsext-full-resolver/dug/dug.hs
+++ dnsext-full-resolver/dug/dug.hs
@@ -13,7 +13,7 @@ main :: IO ()
 main = do
   args <- getArgs
   case take 1 args of
-    [x] | x `elem` ["-h", "--help"]  -> putStr $ help
+    [x] | x `elem` ["-h", "--help"]  -> putStr help
     _                                ->
       either (fail . show) (putStr . pprResult args)
       =<< uncurry (uncurry $ uncurry operate) =<< parseArgs args
