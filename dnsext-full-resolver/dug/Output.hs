7c9bae9daa5d57b24959c88c814ee7e17b5c2b73
Kazu Yamamoto <kazu@iij.ad.jp>
2022-10-25 12:05:37 +0900

    pretty-printing header

--- dnsext-full-resolver/dug/Output.hs
+++ dnsext-full-resolver/dug/Output.hs
@@ -2,11 +2,11 @@
 
 module Output (pprResult) where
 
-import Data.Monoid (Endo (..))
 import Control.Monad.Trans.Writer (Writer, execWriter, tell)
+import Data.Maybe (catMaybes)
+import Data.Monoid (Endo (..))
 
-import DNS.Types (DNSMessage(..), DNSHeader(..), ResourceRecord (..), Question)
-import qualified DNS.Types as DNS
+import DNS.Types
 
 ----------------------------------------------------------------
 
@@ -25,15 +25,50 @@ runPrinter p = ($ "") . appEndo . execWriter . p
 
 result :: Printer DNSMessage
 result DNSMessage{..} = do
-  dsemi *> sp *> string "Header:" *> nl
-  semi *> string (show header) *> nl
+  putHeader header
   nl
-  dsemi *> sp *> string "Opt pseudo section:" *> nl
+  dsemi *> sp *> string "OPTIONAL PSEUDO SECTION:" *> nl
   semi *> string (show ednsHeader) *> nl
   putQS question
   putRRS answers     answer
   putRRS authoritys  authority
   putRRS additionals additional
+
+----------------------------------------------------------------
+
+putHeader :: Printer DNSHeader
+putHeader DNSHeader{..} = do
+  dsemi *> sp *> string "HEADER SECTION:"
+  nl
+  semi *> string (opcd opcode)              *> char ',' *> sp
+  string (show rcode)                       *> char ',' *> sp
+  string "id: " *> string (show identifier)
+  nl
+  semi *> string "Flags:" *> sp *> putFlags flags
+  nl
+  where
+    DNSFlags{..} = flags
+
+putFlags :: Printer DNSFlags
+putFlags DNSFlags{..} = sepBy string (string ", ") $ catMaybes xs
+  where
+    jst x str = if x then Just str else Nothing
+    xs :: [Maybe String]
+    xs = [ jst authAnswer   "Authoritative Answer"
+         , jst trunCation   "Truncated Caution"
+         , jst recDesired   "Recursion Desired"
+         , jst recAvailable "Recursion Available"
+         , jst authenData   "Authenticated Data"
+         , jst chkDisable   "Checking Disabled"
+         ]
+
+opcd :: OPCODE -> String
+opcd OP_STD    = "Standard query"
+opcd OP_INV    = "Inverse query"
+opcd OP_SSR    = "Server status request"
+opcd OP_NOTIFY = "Change notification"
+opcd OP_UPDATE = "Update request"
+opcd x         = show x
 
 ----------------------------------------------------------------
 
@@ -44,13 +79,13 @@ putQS qs = do
   mapM_ qq qs
 
 qq :: Printer Question
-qq q = do
-  semi *> string (DNS.origName $ DNS.qname q)
+qq Question{..} = do
+  semi *> string (origName qname)
   tab
   tab
-  string $ cls $ DNS.qclass q
+  string $ cls qclass
   tab
-  string $ show $ DNS.qtype q
+  string $ show qtype
   nl
 
 ----------------------------------------------------------------
@@ -58,38 +93,38 @@ qq q = do
 putRRS :: Printer [ResourceRecord] -> [ResourceRecord] -> Print ()
 putRRS ppr rs = nl *> ppr rs
 
-answers :: Printer DNS.Answers
+answers :: Printer Answers
 answers = rrs "ANSWER SECTION:"
 
-authoritys :: Printer DNS.AuthorityRecords
+authoritys :: Printer AuthorityRecords
 authoritys = rrs "AUTHORITY SECTION:"
 
-additionals :: Printer DNS.AdditionalRecords
+additionals :: Printer AdditionalRecords
 additionals = rrs "ADDITIONAL SECTION:"
 
-rrs :: String -> Printer [DNS.ResourceRecord]
+rrs :: String -> Printer [ResourceRecord]
 rrs name rs = do
   dsemi *> sp *> string name *> nl
   mapM_ rr rs
 
 rr :: Printer ResourceRecord
-rr r = do
-  string $ DNS.origName $ rrname r
+rr ResourceRecord{..} = do
+  string $ origName rrname
   tab
-  string $ show $ rrttl r
+  string $ show rrttl
   tab
-  string $ cls $ rrclass r
+  string $ cls rrclass
   tab
-  string $ show $ rrtype r
+  string $ show rrtype
   tab
-  string $ show $ rdata r
+  string $ show rdata
   nl
 
 ----------------------------------------------------------------
 
-cls :: DNS.CLASS -> String
-cls c | c == DNS.classIN = "IN"
-      | otherwise        = "#<" ++ show c ++ ">"
+cls :: CLASS -> String
+cls c | c == classIN = "IN"
+      | otherwise    = "#<" ++ show c ++ ">"
 
 ----------------------------------------------------------------
 
