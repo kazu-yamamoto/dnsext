5ac80a349c5b5e4e53c1d1847fec8a7539981a68
Kazu Yamamoto <kazu@iij.ad.jp>
2022-10-27 18:36:46 +0900

    enriching Opaque interface

--- dnsext-types/DNS/Types/Opaque.hs
+++ dnsext-types/DNS/Types/Opaque.hs
@@ -1,9 +1,17 @@
 module DNS.Types.Opaque (
     Opaque
+  , null
+  , singleton
+  , concat
+  , splitAt
+  , uncons
+  , length
   , toByteString
   , fromByteString
   , toShortByteString
   , fromShortByteString
   ) where
+
+import Prelude hiding (null, concat, splitAt, length)
 
 import DNS.Types.Opaque.Internal
--- dnsext-types/DNS/Types/Opaque/Internal.hs
+++ dnsext-types/DNS/Types/Opaque/Internal.hs
@@ -44,6 +44,33 @@ fromShortByteString = Opaque
 
 ----------------------------------------------------------------
 
+instance Semigroup Opaque where
+    Opaque x <> Opaque y = Opaque (x <> y)
+
+null :: Opaque -> Bool
+null (Opaque sbs) = Short.null sbs
+
+singleton :: Word8 -> Opaque
+singleton w = Opaque $ Short.singleton w
+
+concat :: [Opaque] -> Opaque
+concat ss = Opaque $ Short.concat $ map toShortByteString ss
+
+splitAt :: Int -> Opaque -> (Opaque, Opaque)
+splitAt n (Opaque sbs) = (Opaque x, Opaque y)
+  where
+    (x,y) = Short.splitAt n sbs
+
+uncons :: Opaque -> Maybe (Word8, Opaque)
+uncons (Opaque sbs) = case Short.uncons sbs of
+  Just (x,sbs') -> Just (x, Opaque sbs')
+  Nothing       -> Nothing
+
+length :: Opaque -> Int
+length (Opaque sbs) = Short.length sbs
+
+----------------------------------------------------------------
+
 putOpaque :: Opaque -> SPut
 putOpaque (Opaque o) = putShortByteString o
 
@@ -55,7 +82,6 @@ putLenOpaque (Opaque o) =
     -- put the length of the given string
     putInt8 (fromIntegral $ Short.length o)
  <> putShortByteString o
-
 
 getLenOpaque :: SGet Opaque
 getLenOpaque = Opaque <$> (getInt8 >>= getNShortByteString)
