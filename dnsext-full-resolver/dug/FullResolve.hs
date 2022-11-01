f9793d66e5036b1bf433f4f512e69d7663bcccb8
Kei Hibino <ex8k.hibino@gmail.com>
2022-11-01 14:14:21 +0900

    dug: stub to call iterative logic

---
+++ dnsext-full-resolver/dug/FullResolve.hs
@@ -0,0 +1,48 @@
+module FullResolve where
+
+import Control.Concurrent (forkIO)
+import Data.String (fromString)
+import qualified DNS.Types as DNS
+
+import qualified DNS.Cache.Log as Log
+import qualified DNS.Cache.TimeCache as TimeCache
+import qualified DNS.Cache.UpdateCache as UCache
+import DNS.Cache.Iterative (Context (..), QueryError)
+import qualified DNS.Cache.Iterative as Iterative
+
+import DNS.Types
+
+fullResolve :: Bool
+            -> Log.Output
+            -> Log.Level
+            -> String
+            -> TYPE
+            -> IO (Either QueryError DNSMessage)
+fullResolve disableV6NS logOutput logLevel n ty = do
+  (putLines, waitQuitLog, loops, cxt) <- setup disableV6NS logOutput logLevel
+  mapM_ forkIO $ loops
+  out <- resolve cxt n ty
+  putLines Log.INFO ["--------------------"]
+  waitQuitLog
+  return out
+
+setup :: Bool -> Log.Output -> Log.Level -> IO (Log.Level -> [String] -> IO (), IO (), [IO ()], Context)
+setup disableV6NS logOutput logLevel = do
+  (logLoop, putLines, _, waitQuit) <- Log.new (Log.outputHandle logOutput) logLevel
+  tcache <- TimeCache.new
+  (loops, insert, getCache, _, _) <- UCache.new putLines tcache $ 4 * 1024
+  let ucache = (insert, getCache)
+  cxt <- Iterative.newContext putLines disableV6NS ucache tcache
+  return (putLines, waitQuit, logLoop : loops, cxt)
+
+resolve :: Context -> String -> TYPE -> IO (Either QueryError DNSMessage)
+resolve cxt n ty = do
+  fmap toMessage <$> Iterative.runResolve cxt (fromString n) ty
+  where
+    toMessage ((cnRRs, _), e) = either cached response e
+      where
+        cached (rc, ans, auth) = defaultResponse { DNS.header = h { DNS.flags = f }, DNS.answer = cnRRs ans, DNS.authority = auth }
+          where
+            h = DNS.header defaultResponse
+            f = (DNS.flags h) { DNS.rcode = rc }
+        response msg = msg { DNS.answer = cnRRs $ DNS.answer msg }
