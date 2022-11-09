c3d6257dd40a7d77aac16a2077a31c1703f36cd7
Kazu Yamamoto <kazu@iij.ad.jp>
2022-11-09 19:07:26 +0900

    defining instance of SPV

--- dnsext-svcb/DNS/SVCB/Value.hs
+++ dnsext-svcb/DNS/SVCB/Value.hs
@@ -3,7 +3,9 @@ module DNS.SVCB.Value where
 import DNS.Types
 import DNS.Types.Internal
 import qualified DNS.Types.Opaque as Opaque
+import Data.IP
 
+import DNS.SVCB.Imports
 import DNS.SVCB.Key
 
 ----------------------------------------------------------------
@@ -19,22 +21,48 @@ class SPV a where
 encodeSPV :: SPut () -> Opaque
 encodeSPV = Opaque.fromByteString . runSPut
 
-decodeSPV :: SGet a -> Opaque -> Maybe a
-decodeSPV parser o = case runSGet parser bs of
+decodeSPV :: (Int -> SGet a) -> Opaque -> Maybe a
+decodeSPV parser o = case runSGet (parser len) bs of
     Right (r,_) -> Just r
     _           -> Nothing
   where
     bs = Opaque.toByteString o
+    len = Opaque.length o
 
 ----------------------------------------------------------------
 
-newtype SPV_Mandatory = SPV_Moandatory [SvcParamKey] deriving (Eq,Ord,Show)
+newtype SPV_Mandatory = SPV_Mandatory [SvcParamKey] deriving (Eq,Ord,Show)
 
 instance SPV SPV_Mandatory where
-    encodeSvcParamValue (SPV_Moandatory ks) = encodeSPV $ do
-        put16 (fromSvcParamKey SPK_Mandatory)
-        putInt16 (length ks * 2)
+    encodeSvcParamValue (SPV_Mandatory ks) = encodeSPV $
         mapM_ (put16 . fromSvcParamKey) ks
-    decodeSvcParamValue = decodeSPV $ do
-        len <- getInt16
-        SPV_Moandatory <$> sGetMany "Mandatory" len (toSvcParamKey <$> get16)
+    decodeSvcParamValue = decodeSPV $ \len -> do
+        SPV_Mandatory <$> sGetMany "Mandatory" len (toSvcParamKey <$> get16)
+
+----------------------------------------------------------------
+
+newtype SPV_Port = SPV_Port Word16 deriving (Eq,Ord,Show)
+
+instance SPV SPV_Port where
+    encodeSvcParamValue (SPV_Port p) = encodeSPV $ put16 p
+    decodeSvcParamValue = decodeSPV $ \_ -> SPV_Port <$> get16
+
+----------------------------------------------------------------
+
+newtype SPV_IPv4Hint = SPV_IPv4Hint [IPv4] deriving (Eq,Ord,Show)
+
+instance SPV SPV_IPv4Hint where
+    encodeSvcParamValue (SPV_IPv4Hint is) = encodeSPV $ do
+        mapM_ (mapM_ putInt8 . fromIPv4) is
+    decodeSvcParamValue = decodeSPV $ \len -> do
+        SPV_IPv4Hint <$> sGetMany "IPv4Hint" len (toIPv4 <$> getNBytes 4)
+
+----------------------------------------------------------------
+
+newtype SPV_IPv6Hint = SPV_IPv6Hint [IPv6] deriving (Eq,Ord,Show)
+
+instance SPV SPV_IPv6Hint where
+    encodeSvcParamValue (SPV_IPv6Hint is) = encodeSPV $ do
+        mapM_ (mapM_ putInt8 . fromIPv6b) is
+    decodeSvcParamValue = decodeSPV $ \len -> do
+        SPV_IPv6Hint <$> sGetMany "IPv6Hint" len (toIPv6b <$> getNBytes 16)
