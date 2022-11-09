241cc8354f4770261364d152d6922f5c99a829ad
Kei Hibino <ex8k.hibino@gmail.com>
2022-11-09 10:28:46 +0900

    verify-ds: toplevel to verify DS

--- dnsext-dnssec/DNS/SEC/Verify/Verify.hs
+++ dnsext-dnssec/DNS/SEC/Verify/Verify.hs
@@ -8,12 +8,14 @@ import DNS.Types
 
 -- this package
 import DNS.SEC.PubAlg
-import DNS.SEC.Types (RD_RRSIG(..), RD_DNSKEY(..))
+import DNS.SEC.HashAlg
+import DNS.SEC.Types (RD_RRSIG(..), RD_DNSKEY(..), RD_DS (..))
 
-import DNS.SEC.Verify.Types (RRSIGImpl, verifyRRSIGwith)
+import DNS.SEC.Verify.Types (RRSIGImpl, verifyRRSIGwith, DSImpl, verifyDSwith)
 import DNS.SEC.Verify.RSA (rsaSHA1, rsaSHA256, rsaSHA512)
 import DNS.SEC.Verify.ECDSA (ecdsaP256SHA, ecdsaP384SHA)
 import DNS.SEC.Verify.EdDSA (ed25519, ed448)
+import qualified DNS.SEC.Verify.SHA as DS
 
 
 rrsigDicts :: Map PubAlg RRSIGImpl
@@ -35,3 +37,19 @@ verifyRRSIG dnskey rrsig rr =
   where
     alg = dnskey_pubalg dnskey
     verify impl = verifyRRSIGwith impl dnskey rrsig rr
+
+dsDicts :: Map DigestAlg DSImpl
+dsDicts =
+  Map.fromList
+  [ (SHA1   , DS.sha1)
+  , (SHA256 , DS.sha256)
+  , (SHA384 , DS.sha384)
+  ]
+
+verifyDS :: Domain -> RD_DNSKEY -> RD_DS -> Either String ()
+verifyDS owner dnskey ds =
+  maybe (Left $ "verifyDS: unsupported algorithm: " ++ show alg) verify $
+  Map.lookup alg dsDicts
+  where
+    alg = ds_hashalg ds
+    verify impl = verifyDSwith impl owner dnskey ds
