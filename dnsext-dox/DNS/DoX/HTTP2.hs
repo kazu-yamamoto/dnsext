afb7389c26ae0b2e025d0c2bb0b422f47e430fc1
Kazu Yamamoto <kazu@iij.ad.jp>
2022-11-21 15:30:48 +0900

    refactoring

--- dnsext-dox/DNS/DoX/HTTP2.hs
+++ dnsext-dox/DNS/DoX/HTTP2.hs
@@ -9,7 +9,7 @@ import Data.ByteString.Char8 ()
 import qualified Data.ByteString.Char8 as C8
 import Foreign.Marshal.Alloc (mallocBytes, free)
 import Network.HTTP.Types
-import qualified Network.HTTP2.Client as H2
+import Network.HTTP2.Client
 import Network.Socket hiding (recvBuf)
 import Network.Socket.BufferPool
 import Network.TLS hiding (HostName)
@@ -36,41 +36,41 @@ doh hostname port qry = do
 
 client :: Context -> HostName -> ByteString -> IO ()
 client ctx hostname msg =
-    E.bracket (allocConfig ctx 4096) freeConfig $ \conf -> H2.run cliconf conf cli
+    E.bracket (allocConfig ctx 4096) freeConfig $ \conf -> run cliconf conf cli
   where
-    req = H2.requestBuilder methodPost "/dns-query" clientDoHHeaders $ BB.byteString msg
-    cliconf = H2.ClientConfig {
-        H2.scheme = "https"
-      , H2.authority = C8.pack hostname
-      , H2.cacheLimit = 20
+    req = requestBuilder methodPost "/dns-query" clientDoHHeaders $ BB.byteString msg
+    cliconf = ClientConfig {
+        scheme = "https"
+      , authority = C8.pack hostname
+      , cacheLimit = 20
       }
     cli sendRequest = sendRequest req $ \rsp -> do
         bs <- loop rsp ""
         print $ decode bs
       where
         loop rsp bs0 = do
-            bs <- H2.getResponseBodyChunk rsp
+            bs <- getResponseBodyChunk rsp
             if bs == "" then return bs0
                         else loop rsp (bs0 <> bs)
 
-allocConfig :: Context -> Int -> IO H2.Config
+allocConfig :: Context -> Int -> IO Config
 allocConfig ctx bufsiz = do
     buf <- mallocBytes bufsiz
     timmgr <- T.initialize $ 30 * 1000000
     (recv, recvBuf) <- makeRecv $ recvTLS ctx
     recvN <- makeReceiveN "" recv recvBuf
-    let config = H2.Config {
-            H2.confWriteBuffer = buf
-          , H2.confBufferSize = bufsiz
-          , H2.confSendAll = sendTLS ctx
-          , H2.confReadN = recvN
-          , H2.confPositionReadMaker = H2.defaultPositionReadMaker
-          , H2.confTimeoutManager = timmgr
+    let config = Config {
+            confWriteBuffer = buf
+          , confBufferSize = bufsiz
+          , confSendAll = sendTLS ctx
+          , confReadN = recvN
+          , confPositionReadMaker = defaultPositionReadMaker
+          , confTimeoutManager = timmgr
           }
     return config
 
 -- | Deallocating the resource of the simple configuration.
-freeConfig :: H2.Config -> IO ()
+freeConfig :: Config -> IO ()
 freeConfig conf = do
-    free $ H2.confWriteBuffer conf
-    T.killManager $ H2.confTimeoutManager conf
+    free $ confWriteBuffer conf
+    T.killManager $ confTimeoutManager conf
