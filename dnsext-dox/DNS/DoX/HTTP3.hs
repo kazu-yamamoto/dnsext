3780706fcfa36ebcf12aade25e27dae8bb5094e3
Kazu Yamamoto <kazu@iij.ad.jp>
2022-11-21 15:30:48 +0900

    refactoring

--- dnsext-dox/DNS/DoX/HTTP3.hs
+++ dnsext-dox/DNS/DoX/HTTP3.hs
@@ -7,27 +7,27 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Char8 as C8
 import Network.HTTP.Types
-import qualified Network.HTTP3.Client as H3
+import Network.HTTP3.Client
 import Network.QUIC
-import Network.QUIC.Client
+import qualified Network.QUIC.Client as QUIC
 import Network.Socket hiding (recvBuf)
 import qualified UnliftIO.Exception as E
 
 import DNS.DoX.Common
 
 doh3 :: HostName -> PortNumber -> WireFormat -> IO ()
-doh3 hostname port qry = run cc $ \conn -> client conn hostname qry
+doh3 hostname port qry = QUIC.run cc $ \conn -> client conn hostname qry
   where
     cc = getQUICParams hostname port "h3"
 
 client :: Connection -> HostName -> ByteString -> IO ()
-client conn hostname msg = E.bracket H3.allocSimpleConfig H3.freeSimpleConfig $ \conf -> H3.run conn cliconf conf cli
+client conn hostname msg = E.bracket allocSimpleConfig freeSimpleConfig $ \conf -> run conn cliconf conf cli
   where
-    req = H3.requestBuilder methodPost "/dns-query" clientDoHHeaders $ BB.byteString msg
-    cliconf = H3.ClientConfig {
-        H3.scheme = "https"
-      , H3.authority = C8.pack hostname
+    req = requestBuilder methodPost "/dns-query" clientDoHHeaders $ BB.byteString msg
+    cliconf = ClientConfig {
+        scheme = "https"
+      , authority = C8.pack hostname
       }
     cli sendRequest = sendRequest req $ \rsp -> do
-        bs <- H3.getResponseBodyChunk rsp
+        bs <- getResponseBodyChunk rsp
         print $ decode bs
