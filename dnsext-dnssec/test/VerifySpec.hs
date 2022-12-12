73214429ba74b2337d35498a0c093d3768edb258
Kei Hibino <ex8k.hibino@gmail.com>
2022-12-12 20:29:36 +0900

    verify-nsec3: test: add test cases

--- dnsext-dnssec/test/VerifySpec.hs
+++ dnsext-dnssec/test/VerifySpec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module VerifySpec (spec) where
 
@@ -10,6 +11,7 @@ import Data.Int
 import Data.Word
 import Data.ByteString (ByteString)
 
+import Data.ByteString.Base32.Hex (decodeBase32)
 import Data.ByteArray.Encoding (Base (Base16, Base64), convertFromBase)
 
 import DNS.Types
@@ -33,6 +35,17 @@ spec = do
     it "ECDSA/P384" $ caseRRSIG ecdsaP384
     it "Ed25519"    $ caseRRSIG ed25519
     it "Ed448"      $ caseRRSIG ed448
+  describe "NSEC3 hash" $ do
+    it "RFC7129 section5" $ caseNSEC3Hash nsec3HashRFC7129
+  describe "verify NSEC3" $ do
+    it "NameError 1" $ caseNSEC3 nsec3RFC7129NameError
+    it "NameError 2" $ caseNSEC3 nsec3RFC5155NameError
+    it "NoData 1" $ caseNSEC3 nsec3RFC5155NoData1
+    it "NoData 2" $ caseNSEC3 nsec3RFC5155NoData2
+    it "NoData 3" $ caseNSEC3 nsec3RFC5155NoData3
+    it "opt-out delegation" $ caseNSEC3 nsec3RFC5155OptOut
+    it "wildcard expansion" $ caseNSEC3 nsec3RFC5155WildcardExpansion
+    it "wildcard NoData" $ caseNSEC3 nsec3RFC5155WildcardNoData
 
 -----
 -- KeyTag cases
@@ -232,6 +245,195 @@ ed448 =
              \ SOIOKBGzG+yWYtz1/jjbzl5GGkWvREUCUA "
 
 -----
+-- NSEC3 hash cases
+
+type NSEC3Hash_CASE = ((RData, Domain), String)
+
+caseNSEC3Hash :: NSEC3Hash_CASE -> Expectation
+caseNSEC3Hash ((rd, domain), expectB32H) = either expectationFailure (const $ pure ()) $ do
+  nsec3 <- maybe (Left $ "caseNSEC3Hash: not NSEC3: " ++ show rd) Right $ fromRData rd
+  computed <- hashNSEC3 nsec3 domain
+  check computed
+  where
+    expect = opaqueFromB32Hex expectB32H
+    check computed
+      | computed == expect  =  Right ()
+      | otherwise           =  Left $ show computed ++ " =/= " ++ show expect
+
+-- example from https://datatracker.ietf.org/doc/html/rfc7129#section-5
+nsec3HashRFC7129 :: NSEC3Hash_CASE
+nsec3HashRFC7129 =
+  ( ( rd_nsec3' 1 0 2 "DEAD" "1AVVQN74SG75UKFVF25DGCETHGQ638EK" [NS, SOA, RRSIG, DNSKEY, NSEC3PARAM]
+    , "example.org."
+    )
+  , "15bg9l6359f5ch23e34ddua6n1rihl9h"
+  )
+
+-----
+-- NSEC3 cases
+
+type NSEC3_ExpectW = (Domain, Domain) {- expect owner and qname -}
+data NSEC3_Expect
+  = N3W_NameError NSEC3_ExpectW NSEC3_ExpectW NSEC3_ExpectW
+  | N3W_NoData NSEC3_ExpectW
+  | N3W_OptOutDelegation NSEC3_ExpectW NSEC3_ExpectW
+  | N3W_WildcardExpansion NSEC3_ExpectW
+  | N3W_WildcardNoData NSEC3_ExpectW NSEC3_ExpectW NSEC3_ExpectW
+  deriving (Eq, Show)
+type NSEC3_CASE = (([(Domain, RData)], Domain, TYPE), NSEC3_Expect)
+
+nsec3CheckResult :: NSEC3_Result -> NSEC3_Expect -> Either String ()
+nsec3CheckResult result expect = case (result, expect) of
+  (N3Result_NameError {..}, N3W_NameError ec en ew)               -> do
+    check "name-error: closest"         (w2e nsec3_closest_match) ec
+    check "name-error: next"            (w2e nsec3_next_closer_cover) en
+    check "name-error: wildcard"        (w2e nsec3_wildcard_cover) ew
+  (N3Result_NoData {..},    N3W_NoData ec)                        -> do
+    check "no-data: closest"            (w2e nsec3_closest_match) ec
+  (N3Result_OptOutDelegation {..}, N3W_OptOutDelegation ec en)    -> do
+    check "opt-out: closest"            (w2e nsec3_closest_match) ec
+    check "opt-out: next"               (w2e nsec3_next_closer_cover) en
+  (N3Result_WildcardExpansion {..}, N3W_WildcardExpansion en)     -> do
+    check "wildcard-expansion: next"    (w2e nsec3_next_closer_cover) en
+  (N3Result_WildcardNoData {..},    N3W_WildcardNoData ec en ew)  -> do
+    check "wildcard-no-data: closest"   (w2e nsec3_closest_match) ec
+    check "wildcard-no-data: next"      (w2e nsec3_next_closer_cover) en
+    check "wildcard-no-data: wildcard"  (w2e nsec3_wildcard_match) ew
+  _                                                               ->
+    Left $ unlines ["result data mismatch:", show result, show expect]
+
+  where
+    w2e :: NSEC3_Witness -> NSEC3_ExpectW
+    w2e ((owner, _range), qname) = (owner, qname)
+    check tag r e
+      | r == e     =  Right ()
+      | otherwise  =  Left $ tag ++ ": " ++ show r ++ " =/= " ++ show e
+
+caseNSEC3 :: NSEC3_CASE -> Expectation
+caseNSEC3 ((rds, qn, qtype), expect) = either expectationFailure (const $ pure ()) $ do
+  result <- verifyNSEC3 ranges qn qtype
+  nsec3CheckResult result expect
+  where
+    ranges = [ (owner, nsec3) | (owner, rd) <- rds, Just nsec3 <- [fromRData rd] ]
+
+-- example from https://datatracker.ietf.org/doc/html/rfc7129#section-5.5
+nsec3RFC7129NameError :: NSEC3_CASE
+nsec3RFC7129NameError = ((rdatas, fromString "x.2.example.org.", TXT), expect)
+  where
+    rdatas =
+      [ ("15bg9l6359f5ch23e34ddua6n1rihl9h.example.org.",
+         rd_nsec3' 1 0 2 "DEAD" "1AVVQN74SG75UKFVF25DGCETHGQ638EK" [NS, SOA, RRSIG, DNSKEY, NSEC3PARAM])
+      , ("1avvqn74sg75ukfvf25dgcethgq638ek.example.org.",
+         rd_nsec3' 1 0 2 "DEAD" "75B9ID679QQOV6LDFHD8OCSHSSSB6JVQ" [])
+      , ("75b9id679qqov6ldfhd8ocshsssb6jvq.example.org.",
+         rd_nsec3' 1 0 2 "DEAD" "8555T7QEGAU7PJTKSNBCHG4TD2M0JNPJ" [TXT, RRSIG])
+      ]
+    expect =
+      N3W_NameError
+      ("15bg9l6359f5ch23e34ddua6n1rihl9h.example.org.", "example.org.")
+      ("75b9id679qqov6ldfhd8ocshsssb6jvq.example.org.", "2.example.org.")
+      ("1avvqn74sg75ukfvf25dgcethgq638ek.example.org.", "*.example.org.")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.1
+-- Name Error
+nsec3RFC5155NameError :: NSEC3_CASE
+nsec3RFC5155NameError = ((rdatas, "a.c.x.w.example.", A), expect)
+  where
+    rdatas =
+      [ ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "2t7b4g4vsa5smi47k61mv5bv1a22bojr" [MX, DNSKEY, NS, SOA, NSEC3PARAM, RRSIG])
+      , ("b4um86eghhds6nea196smvmlo4ors995.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "gjeqe526plbf1g8mklp59enfd789njgi" [MX, RRSIG])
+      , ("35mthgpgcu1qg68fab165klnsnk3dpvl.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "b4um86eghhds6nea196smvmlo4ors995" [NS, DS, RRSIG])
+      ]
+    expect =
+      N3W_NameError
+      ("b4um86eghhds6nea196smvmlo4ors995.example.", "x.w.example.")
+      ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.", "c.x.w.example.")
+      ("35mthgpgcu1qg68fab165klnsnk3dpvl.example.", "*.x.w.example")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.2
+-- No Data Error
+nsec3RFC5155NoData1 :: NSEC3_CASE
+nsec3RFC5155NoData1 = ((rdatas, "ns1.example.", MX), expect)
+  where
+    rdatas =
+      [ ("2t7b4g4vsa5smi47k61mv5bv1a22bojr.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "2vptu5timamqttgl4luu9kg21e0aor3s" [A, RRSIG])
+      ]
+    expect = N3W_NoData ("2t7b4g4vsa5smi47k61mv5bv1a22bojr.example.", "ns1.example.")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.2.1
+-- No Data Error, Empty Non-Terminal
+nsec3RFC5155NoData2 :: NSEC3_CASE
+nsec3RFC5155NoData2 = ((rdatas, "y.w.example.", A), expect)
+  where
+    rdatas =
+      [ ("ji6neoaepv8b5o6k4ev33abha8ht9fgc.example.",
+        rd_nsec3' 1 1 12 "aabbccdd" "k8udemvp1j2f7eg6jebps17vp3n8i58h" [])
+      ]
+    expect = N3W_NoData ("ji6neoaepv8b5o6k4ev33abha8ht9fgc.example.", "y.w.example.")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.6
+-- DS Child Zone No Data Error
+nsec3RFC5155NoData3 :: NSEC3_CASE
+nsec3RFC5155NoData3 = ((rdatas, "example.", DS), expect)
+  where
+    rdatas =
+      [ ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "2t7b4g4vsa5smi47k61mv5bv1a22bojr" [MX, DNSKEY, NS])
+      ]
+    expect = N3W_NoData ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.", "example.")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.3
+-- Referral to an Opt-Out Unsigned Zone
+nsec3RFC5155OptOut :: NSEC3_CASE
+nsec3RFC5155OptOut = ((rdatas, "mc.c.example.", MX), expect)
+  where
+    rdatas =
+      [ ("35mthgpgcu1qg68fab165klnsnk3dpvl.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "b4um86eghhds6nea196smvmlo4ors995" [NS, DS, RRSIG])
+      , ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "2t7b4g4vsa5smi47k61mv5bv1a22bojr" [MX, DNSKEY, NS, SOA, NSEC3PARAM, RRSIG])
+      ]
+    expect =
+      N3W_OptOutDelegation
+      ("0p9mhaveqvm6t7vbl5lop2u3t2rp3tom.example.", "example.")
+      ("35mthgpgcu1qg68fab165klnsnk3dpvl.example.", "c.example.")
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.4
+-- Wildcard Expansion
+nsec3RFC5155WildcardExpansion :: NSEC3_CASE
+nsec3RFC5155WildcardExpansion = ((rdatas, "a.z.w.example.", MX), expect)
+  where
+    rdatas =
+      [ ("q04jkcevqvmu85r014c7dkba38o0ji5r.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "r53bq7cc2uvmubfu5ocmm6pers9tk9en" [A, RRSIG])
+      ]
+    expect = N3W_WildcardExpansion ("q04jkcevqvmu85r014c7dkba38o0ji5r.example.", "z.w.example.")
+
+
+-- example from https://datatracker.ietf.org/doc/html/rfc5155#appendix-B.5
+-- Wildcard No Data Error
+nsec3RFC5155WildcardNoData :: NSEC3_CASE
+nsec3RFC5155WildcardNoData = ((rdatas, "a.z.w.example.", AAAA), expect)
+  where
+    rdatas =
+      [ ("k8udemvp1j2f7eg6jebps17vp3n8i58h.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "kohar7mbb8dc2ce8a9qvl8hon4k53uhi" [])
+      , ("q04jkcevqvmu85r014c7dkba38o0ji5r.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "r53bq7cc2uvmubfu5ocmm6pers9tk9en" [A, RRSIG])
+      , ("r53bq7cc2uvmubfu5ocmm6pers9tk9en.example.",
+         rd_nsec3' 1 1 12 "aabbccdd" "t644ebqk9bibcna874givr6joj62mlhv" [MX, RRSIG])
+      ]
+    expect =
+      N3W_WildcardNoData
+      ("k8udemvp1j2f7eg6jebps17vp3n8i58h.example.", "w.example.")
+      ("q04jkcevqvmu85r014c7dkba38o0ji5r.example.", "z.w.example.")
+      ("r53bq7cc2uvmubfu5ocmm6pers9tk9en.example.", "*.w.example")
+
+-----
 -- helpers
 
 rd_dnskey' :: Word16 -> Word8 -> Word8 -> String -> RData
@@ -245,10 +447,18 @@ rd_ds' keytag pubalg digalg digest = rd_ds keytag (toPubAlg pubalg) (toDigestAlg
 rd_rrsig' :: TYPE -> Word8 -> Word8 -> TTL -> Int64 -> Int64 -> Word16 -> String -> String -> RData
 rd_rrsig' typ alg a b c d e dom = rd_rrsig typ (toPubAlg alg) a b c d e (fromString dom) . opaqueFromB64
 
+rd_nsec3' :: Word8 -> Word8 -> Word16 -> String -> String -> [TYPE] -> RData
+rd_nsec3' alg fs i salt next = rd_nsec3 (toHashAlg alg) (toNSEC3flags fs) i (opaqueFromB16Hex salt) (opaqueFromB32Hex next)
+
 opaqueFromB16Hex :: String -> Opaque
 opaqueFromB16Hex =
   either (error "opaqueFromB16Hex: fail to decode hex") Opaque.fromByteString .
   convertFromBase Base16 . (fromString :: String -> ByteString) . filter (/= ' ')
+
+opaqueFromB32Hex :: String -> Opaque
+opaqueFromB32Hex =
+  either (error "opaqueFromB32Hex: fail to decode base32hex") Opaque.fromByteString .
+  decodeBase32 . (fromString :: String -> ByteString) . filter (/= ' ')
 
 opaqueFromB64 :: String -> Opaque
 opaqueFromB64 =
