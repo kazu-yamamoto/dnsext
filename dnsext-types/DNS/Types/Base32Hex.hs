ec01611ff82dabda9690c2cf0c567729be7811c1
Kei Hibino <ex8k.hibino@gmail.com>
2022-12-19 17:30:53 +0900

    base-encoding: base32hex decoder

--- dnsext-types/DNS/Types/Base32Hex.hs
+++ dnsext-types/DNS/Types/Base32Hex.hs
@@ -1,4 +1,7 @@
-module DNS.Types.Base32Hex (encode) where
+module DNS.Types.Base32Hex (
+    encode
+  , decode
+  ) where
 
 import qualified Data.Array.MArray as A
 import qualified Data.Array.IArray as A
@@ -9,6 +12,7 @@ import qualified Data.ByteString   as BS
 import Control.Monad
 import Data.Bits
 import Data.ByteString (ByteString)
+import qualified Data.List as L
 
 -- | Encode ByteString using the
 -- <https://tools.ietf.org/html/rfc4648#section-7 RFC4648 base32hex>
@@ -51,3 +55,82 @@ encode bs =
         when (r > 2) $ store8 a (q+2) wr
         go ws a $ n + 8
 {-# INLINE encode #-}
+
+decode :: ByteString -> Either String ByteString
+decode bs = do
+  let len = (5 * BS.length bs) `div` 8
+  cs <- fmap chunks8 . mapM fromHex32 $ BS.unpack bs
+  return $ BS.pack $ A.elems $ A.runSTUArray $ do
+    a <- A.newArray (0 :: Int, len-1) 0
+    go cs a 0
+  where
+    fromHex32 w
+      {- '0' <= w <= '9' -}
+      | 48 <= w && w <= 57   =  Right $ w - 48
+      {- 'A' <= w <= 'V' -}
+      | 65 <= w && w <= 86   =  Right $ w - 55
+      {- 'a' <= w <= 'v' -}
+      | 97 <= w && w <= 118  =  Right $ w - 87
+      | otherwise            =  Left "Base32Hex.decode: not base32hex format"
+
+    chunks8 = L.unfoldr chunk
+      where
+        chunk s
+          | null hd    =  Nothing
+          | otherwise  =  Just (hd, tl)
+          where (hd, tl) = splitAt 8 s
+
+    store8 a i v = A.writeArray a i v
+
+    stores ss a n f0 f1 f2 f3 f4 f5 f6 f7 = do
+      let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+          w1 = f1 `shiftL` 6 .|. f2 `shiftL` 1 .|. f3 `shiftR` 4
+          w2 = f3 `shiftL` 4 .|. f4 `shiftR` 1
+          w3 = f4 `shiftL` 7 .|. f5 `shiftL` 2 .|. f6 `shiftR` 3
+          w4 = f6 `shiftL` 5 .|. f7
+      sequence_ $
+        take ss
+        [ store8 a  n      w0,
+          store8 a (n + 1) w1,
+          store8 a (n + 2) w2,
+          store8 a (n + 3) w3,
+          store8 a (n + 4) w4 ]
+
+    go [] a _n =  return a
+    go ([f0,f1]:[]) a n = do
+      stores 1 a n f0 f1 0 0 0 0 0 0
+      -- let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+      return a
+
+    go ([f0,f1,f2,f3]:[]) a n = do
+      stores 2 a n f0 f1 f2 f3 0 0 0 0
+      -- let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+      --     w1 = f1 `shiftL` 6 .|. f2 `shiftL` 1 .|. f3 `shiftR` 4
+      return a
+
+    go ([f0,f1,f2,f3,f4]:[]) a n = do
+      stores 3 a n f0 f1 f2 f3 f4 0 0 0
+      -- let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+      --     w1 = f1 `shiftL` 6 .|. f2 `shiftL` 1 .|. f3 `shiftR` 4
+      --     w2 = f3 `shiftL` 4 .|. f4 `shiftR` 1
+      return a
+
+    go ([f0,f1,f2,f3,f4,f5,f6]:[]) a n = do
+      stores 4 a n f0 f1 f2 f3 f4 f5 f6 0
+      -- let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+      --     w1 = f1 `shiftL` 6 .|. f2 `shiftL` 1 .|. f3 `shiftR` 4
+      --     w2 = f3 `shiftL` 4 .|. f4 `shiftR` 1
+      --     w3 = f4 `shiftL` 7 .|. f5 `shiftL` 2 .|. f6 `shiftR` 3
+      return a
+
+    go ([f0,f1,f2,f3,f4,f5,f6,f7]:cs) a n = do
+      stores 5 a n f0 f1 f2 f3 f4 f5 f6 f7
+      -- let w0 = f0 `shiftL` 3 .|. f1 `shiftR` 2
+      --     w1 = f1 `shiftL` 6 .|. f2 `shiftL` 1 .|. f3 `shiftR` 4
+      --     w2 = f3 `shiftL` 4 .|. f4 `shiftR` 1
+      --     w3 = f4 `shiftL` 7 .|. f5 `shiftL` 2 .|. f6 `shiftR` 3
+      --     w4 = f6 `shiftL` 5 .|. f7
+      go cs a (n + 5)
+
+    go _ _ _ = error "Base32Hex.decode: internal error. invalid chunk"
+{-# INLINE decode #-}
