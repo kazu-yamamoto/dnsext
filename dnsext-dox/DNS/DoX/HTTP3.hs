ab29fda45b9a7a4cc1dc367077b6988b2ceb899a
Kazu Yamamoto <kazu@iij.ad.jp>
2023-01-17 15:07:40 +0900

    refactoring

--- dnsext-dox/DNS/DoX/HTTP3.hs
+++ dnsext-dox/DNS/DoX/HTTP3.hs
@@ -11,6 +11,7 @@ import qualified Data.ByteString.Builder as BB
 import qualified Data.ByteString.Char8 as C8
 import Network.HTTP.Types
 import Network.HTTP3.Client
+import Network.QUIC
 import qualified Network.QUIC.Client as QUIC
 import qualified UnliftIO.Exception as E
 
@@ -20,20 +21,20 @@ http3Solver :: Solver
 http3Solver si@SolvInfo{..} = QUIC.run cc $ \conn ->
     E.bracket allocSimpleConfig freeSimpleConfig $ \conf -> do
         ident <- solvGenId
-        run conn cliconf conf $ client ident si
+        client conn conf ident si
   where
     cc = getQUICParams solvHostName solvPortNumber "h3"
-    cliconf = ClientConfig {
-        scheme = "https"
-      , authority = C8.pack solvHostName
-      }
 
-client :: Identifier -> SolvInfo -> Client DNSMessage
-client ident SolvInfo{..} = cli
+client :: Connection -> Config -> Identifier -> Solver
+client conn conf ident SolvInfo{..} = run conn cliconf conf cli
   where
     wire = encodeQuery ident solvQuestion solvQueryControls
     hdr = clientDoHHeaders wire
     req = requestBuilder methodPost "/dns-query" hdr $ BB.byteString wire
+    cliconf = ClientConfig {
+        scheme = "https"
+      , authority = C8.pack solvHostName
+      }
     cli sendRequest = sendRequest req $ \rsp -> do
         bs <- loop rsp ""
         now <- solvGetTime
