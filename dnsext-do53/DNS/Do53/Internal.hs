67d4f673e9067d4d5f429dca8a3f995c07e3d3ab
Kazu Yamamoto <kazu@iij.ad.jp>
2023-01-20 14:54:09 +0900

    stop exporting getEpochTime

--- dnsext-do53/DNS/Do53/Internal.hs
+++ dnsext-do53/DNS/Do53/Internal.hs
@@ -24,12 +24,9 @@ module DNS.Do53.Internal (
   , tcpResolver
   , vcResolver
   , checkRespM
-    -- * Misc
-  , getEpochTime
   ) where
 
 import DNS.Do53.Do53
 import DNS.Do53.IO
-import DNS.Do53.Imports
 import DNS.Do53.Resolve
 import DNS.Do53.Types
--- dnsext-dox/DNS/DoX/Internal.hs
+++ dnsext-dox/DNS/DoX/Internal.hs
@@ -1,13 +1,10 @@
 module DNS.DoX.Internal (
-    ResolvInfo(..)
-  , getEpochTime
-  , http2Resolver
+    http2Resolver
   , http3Resolver
   , tlsResolver
   , quicResolver
   )  where
 
-import DNS.Do53.Internal
 import DNS.DoX.HTTP2
 import DNS.DoX.HTTP3
 import DNS.DoX.QUIC
