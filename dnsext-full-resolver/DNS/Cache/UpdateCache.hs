440e1336d8abbb7524af87eebaa8644a6b793bf2
Kei Hibino <ex8k.hibino@gmail.com>
2023-01-22 13:15:27 +0900

    speparate direct deps on the Log module

--- dnsext-full-resolver/DNS/Cache/UpdateCache.hs
+++ dnsext-full-resolver/DNS/Cache/UpdateCache.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module DNS.Cache.UpdateCache (
   new,
   none,
@@ -19,9 +21,17 @@ import UnliftIO (tryAny)
 -- this package
 import DNS.Cache.Queue (newQueue, readQueue, writeQueue)
 import qualified DNS.Cache.Queue as Queue
-import qualified DNS.Cache.Log as Log
 import DNS.Cache.Cache (Cache, Key, CRSet, Ranking)
 import qualified DNS.Cache.Cache as Cache
+
+data CacheConf = CacheConf {
+    memoActions :: MemoActions
+  }
+
+data MemoActions = MemoActions {
+    memoLogLn :: String -> IO ()
+  , memoErrorLn :: String -> IO ()
+  }
 
 data Update
   = I Key TTL CRSet Ranking
@@ -36,39 +46,40 @@ runUpdate t u cache = case u of
 
 type Insert = Key -> TTL -> CRSet -> Ranking -> IO ()
 
-new :: (Log.Level -> [String] -> IO ()) -> (IO EpochTime, IO ShowS)
+new :: CacheConf
+    -> IO EpochTime
     -> Int
     -> IO ([IO ()], Insert, IO Cache, EpochTime -> IO (), IO (Int, Int))
-new putLines (getSec, getTimeStr) maxCacheSize = do
-  let putLn level = putLines level . (:[])
+new CacheConf{..} getSec maxCacheSize = do
+  let MemoActions{..} = memoActions
   cacheRef <- newIORef $ Cache.empty maxCacheSize
 
-  let update1 (ts, tstr, u) = do   -- step of single update theard
+  let update1 (ts, u) = do   -- step of single update theard
         cache <- readIORef cacheRef
         let updateRef c = do
               -- use atomicWrite to guard from out-of-order effect. to propagate updates to other CPU
               c `seq` atomicWriteIORef cacheRef c
               case u of
                 I {}  ->  return ()
-                E     ->  putLn Log.NOTICE $ tstr $ ": some records expired: size = " ++ show (Cache.size c)
+                E     ->  memoLogLn $ "some records expired: size = " ++ show (Cache.size c)
         maybe (pure ()) updateRef $ runUpdate ts u cache
 
   (updateLoop, enqueueU, readUSize) <- do
     inQ <- newQueue 8
-    let errorLn = putLn Log.NOTICE . ("UpdateCache.updateLoop: error: " ++) . show
+    let errorLn = memoErrorLn . ("Memo.updateLoop: error: " ++) . show
         body = either errorLn return =<< tryAny (update1 =<< readQueue inQ)
     return (forever body, writeQueue inQ, (,) <$> (fst <$> Queue.readSizes inQ) <*> pure (Queue.sizeMaxBound inQ))
 
-  let expires1 ts = enqueueU =<< (,,) ts <$> getTimeStr <*> pure E
+  let expires1 ts = enqueueU =<< (,) ts <$> pure E
 
       expireEvsnts = forever body
         where
-          errorLn = putLn Log.NOTICE . ("UpdateCache.expireEvents: error: " ++) . show
+          errorLn = memoErrorLn . ("Memo.expireEvents: error: " ++) . show
           interval = threadDelay $ 1800 * 1000 * 1000  -- when there is no insert for a long time
           body = either errorLn return =<< tryAny (interval *> (expires1 =<< getSec))
 
   let insert k ttl crs rank =
-        enqueueU =<< (,,) <$> getSec <*> getTimeStr <*> pure (I k ttl crs rank)
+        enqueueU =<< (,) <$> getSec <*> pure (I k ttl crs rank)
 
   return ([updateLoop, expireEvsnts], insert, readIORef cacheRef, expires1, readUSize)
 
