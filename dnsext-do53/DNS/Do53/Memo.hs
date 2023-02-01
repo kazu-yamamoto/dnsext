7195f3dabe5711e4a37f17defa419d03eb7d6767
Kazu Yamamoto <kazu@iij.ad.jp>
2023-02-01 13:33:55 +0900

    using reaperMergable

--- dnsext-do53/DNS/Do53/Memo.hs
+++ dnsext-do53/DNS/Do53/Memo.hs
@@ -26,11 +26,12 @@ newtype Cache = Cache (R.Reaper DB (Key,Prio,Entry))
 
 newCache :: Int -> IO Cache
 newCache delay = Cache <$> R.mkReaper R.defaultReaperSettings {
-    R.reaperEmpty  = PSQ.empty
-  , R.reaperCons   = \(k, tim, v) psq -> PSQ.insert k tim v psq
-  , R.reaperAction = prune
-  , R.reaperDelay  = delay * 1000000
-  , R.reaperNull   = PSQ.null
+    R.reaperEmpty    = PSQ.empty
+  , R.reaperCons     = \(k, tim, v) psq -> PSQ.insert k tim v psq
+  , R.reaperAction   = mkPrune
+  , R.reaperDelay    = delay * 1000000
+  , R.reaperNull     = PSQ.null
+  , R.reaperMergable = False
   }
 
 lookupCache :: Key -> Cache -> IO (Maybe (Prio, Entry))
@@ -39,13 +40,8 @@ lookupCache key (Cache reaper) = PSQ.lookup key <$> R.reaperRead reaper
 insertCache :: Key -> Prio -> Entry -> Cache -> IO ()
 insertCache key tim ent (Cache reaper) = R.reaperAdd reaper (key,tim,ent)
 
--- Theoretically speaking, atMostView itself is good enough for pruning.
--- But auto-update assumes a list based db which does not provide atMost
--- functions. So, we need to do this redundant way.
-prune :: DB -> IO (DB -> DB)
-prune oldpsq = do
+mkPrune :: DB -> IO (DB -> DB)
+mkPrune _db = do
     tim <- getEpochTime
-    let (_, pruned) = PSQ.atMostView tim oldpsq
-    return $ \newpsq -> foldl' ins pruned $ PSQ.toList newpsq
-  where
-    ins psq (k,p,v) = PSQ.insert k p v psq
+    let prune = snd . PSQ.atMostView tim
+    return prune
