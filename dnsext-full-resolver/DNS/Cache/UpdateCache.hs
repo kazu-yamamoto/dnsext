064e7de32bfcafc9b1670ca84ddaebe1bcefad40
Kei Hibino <ex8k.hibino@gmail.com>
2023-02-02 16:40:51 +0900

    full-resolver cache with OneShot

--- dnsext-full-resolver/DNS/Cache/UpdateCache.hs
+++ dnsext-full-resolver/DNS/Cache/UpdateCache.hs
@@ -11,14 +11,14 @@ module DNS.Cache.UpdateCache (
   ) where
 
 -- GHC packages
-import Control.Monad (forever)
-import Control.Concurrent (threadDelay)
+import Control.Monad (forever, unless)
 import Control.Concurrent.Chan (newChan, readChan, writeChan)
 import Data.IORef (newIORef, readIORef, atomicWriteIORef)
 
 -- dnsext-* packages
 import DNS.Types (TTL)
 import DNS.Types.Decode (EpochTime)
+import DNS.Do53.OneShot (defaultOneShotSettings, oneShotAction, oneShotDelay, mkOneShot, oneShotRegister)
 
 -- other packages
 import UnliftIO (tryAny)
@@ -58,29 +58,31 @@ new CacheConf{..} = do
   let MemoActions{..} = memoActions
   cacheRef <- newIORef $ Cache.empty maxCacheSize
 
-  let update1 :: UpdateEvent -> IO ()
+  let expires1 ts = memoWriteQueue (Cache.expires ts, expiredLog)
+        where
+          expiredLog c = memoLogLn $ "some records expired: size = " ++ show (Cache.size c)
+
+  oneShotExpire <- mkOneShot defaultOneShotSettings
+                   { oneShotAction = const (expires1 =<< memoGetTime)
+                   , oneShotDelay = 1800 * 1000 * 1000
+                   }
+
+  let registerExpire c = unless (Cache.null c) $ oneShotRegister oneShotExpire
+      update1 :: UpdateEvent -> IO ()
       update1 (uevent, logAction) = do   -- step of single update theard
         cache <- readIORef cacheRef
         let updateRef c = do
               -- use atomicWrite to guard from out-of-order effect. to propagate updates to other CPU
               c `seq` atomicWriteIORef cacheRef c
               logAction c
-        maybe (pure ()) updateRef $ uevent cache
+              registerExpire c
+        maybe (registerExpire cache) updateRef $ uevent cache
 
   updateLoop <- do
     let errorLn = memoErrorLn . ("Memo.updateLoop: error: " ++) . show
         body = either errorLn return =<< tryAny (update1 =<< memoReadQueue)
     return $ forever body
 
-  let expires1 ts = memoWriteQueue (Cache.expires ts, expiredLog)
-        where
-          expiredLog c = memoLogLn $ "some records expired: size = " ++ show (Cache.size c)
-
-      expireEvsnts = forever body
-        where
-          errorLn = memoErrorLn . ("Memo.expireEvents: error: " ++) . show
-          interval = threadDelay $ 1800 * 1000 * 1000  -- when there is no insert for a long time
-          body = either errorLn return =<< tryAny (interval *> (expires1 =<< memoGetTime))
 
   let insert k ttl crs rank = do
         t <- memoGetTime
@@ -88,7 +90,7 @@ new CacheConf{..} = do
             evInsert cache = maybe (insert_ cache) insert_ $ Cache.expires t cache {- expires before insert -}
         memoWriteQueue (evInsert, const $ pure ())
 
-  return ([updateLoop, expireEvsnts], insert, readIORef cacheRef, expires1)
+  return ([updateLoop], insert, readIORef cacheRef, expires1)
 
 -- no caching
 none :: (Insert, IO Cache)
