6b81bcb0be97efd998c7ceb2ef9a77c7ca0044b7
Kei Hibino <ex8k.hibino@gmail.com>
2023-03-13 09:59:34 +0900

    along with review. apply OverloadedStrings

--- dnsext-full-resolver/DNS/Cache/RootTrustAnchors.hs
+++ dnsext-full-resolver/DNS/Cache/RootTrustAnchors.hs
@@ -1,10 +1,9 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 
 module DNS.Cache.RootTrustAnchors (
   rootSepDS
   ) where
 
-import Data.String (fromString)
 import qualified DNS.Types.Opaque as Opaque
 import DNS.SEC
 
@@ -15,5 +14,4 @@ rootSepDS = RD_DS 20326 (PubAlg 8) (DigestAlg 2) digest
   where
     digest =
       either (error . ("rootSepDS: bad configuration: " ++)) id
-      $ Opaque.fromBase16
-      $ fromString "E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D"
+      $ Opaque.fromBase16 "E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC683457104237C7F8EC8D"
