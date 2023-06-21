0a6bb43c5db2d935f872cea36bb9a68c14fffb6a
Kazu Yamamoto <kazu@iij.ad.jp>
2023-06-21 17:08:15 +0900

    extracting Verify

--- dnsext-full-resolver/DNS/Cache/Iterative/Old.hs
+++ dnsext-full-resolver/DNS/Cache/Iterative/Old.hs
@@ -10,7 +10,6 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except (ExceptT (..), runExceptT, throwE)
 import Control.Monad.Trans.Reader (ReaderT (..), asks)
-import DNS.Types.Decode (EpochTime)
 import Data.Function (on)
 import Data.Functor (($>))
 import Data.IORef (atomicWriteIORef, readIORef)
@@ -55,12 +54,9 @@ import DNS.SEC (
     RD_DS (..),
     RD_RRSIG (..),
     TYPE (DNSKEY, DS, NSEC, NSEC3, RRSIG),
-    fromDNSTime,
-    toDNSTime,
  )
 import qualified DNS.SEC.Verify as SEC
 import DNS.Types (
-    CLASS,
     DNSHeader,
     DNSMessage,
     Domain,
@@ -81,6 +77,7 @@ import DNS.Cache.Iterative.Random
 import DNS.Cache.Iterative.Rev
 import DNS.Cache.Iterative.Types
 import DNS.Cache.Iterative.Utils
+import DNS.Cache.Iterative.Verify
 import DNS.Cache.RootServers (rootServers)
 import DNS.Cache.RootTrustAnchors (rootSepDS)
 import DNS.Cache.Types (NE)
@@ -1076,39 +1073,6 @@ verifySEP dss dom rrs = do
   where
     verifyError s = "verifySEP: " ++ s
 
-{- `left` is not RRset case. `right` is just RRset case.
-   `[RD_RRSIG]` is not null on verification success case. -}
-withVerifiedRRset
-    :: EpochTime
-    -> [RD_DNSKEY]
-    -> [ResourceRecord]
-    -> [(RD_RRSIG, TTL)]
-    -> ([ResourceRecord] -> String -> a)
-    -> ( [ResourceRecord] -> Domain -> TYPE -> CLASS -> TTL -> [RData] -> [RD_RRSIG] -> a
-       )
-    -> a
-withVerifiedRRset now dnskeys rrs sigs left right =
-    either (left sortedRRs) ($ rightK) $ SEC.canonicalRRsetSorted sortedRRs
-  where
-    rightK dom typ cls ttl rds = right sortedRRs dom typ cls minTTL rds sigrds
-      where
-        goodSigs =
-            [ rrsig
-            | rrsig@(sigrd, _) <- sigs
-            , key <- dnskeys
-            , Right () <-
-                [SEC.verifyRRSIGsorted (toDNSTime now) key sigrd typ ttl sortedWires]
-            ]
-        (sigrds, sigTTLs) = unzip goodSigs
-        expireTTLs =
-            [ exttl | sig <- sigrds, let exttl = fromDNSTime (rrsig_expiration sig) - now, exttl > 0
-            ]
-        minTTL = minimum $ ttl : sigTTLs ++ map fromIntegral expireTTLs
-    (sortedWires, sortedRRs) = unzip $ SEC.sortCanonical rrs
-
-rrsetEmpty :: RRset
-rrsetEmpty = RRset "" (DNS.toTYPE 0) 0 0 [] []
-
 rrsetNull :: RRset -> Bool
 rrsetNull = null . rrsRDatas
 
@@ -1130,28 +1094,6 @@ rrListFromRRset reqDO RRset{..} = case reqDO of
         [ ResourceRecord rrsName RRSIG rrsClass rrsTTL (DNS.toRData sig)
         | sig <- rrsGoodSigs
         ]
-
-verifyAndCache
-    :: [RD_DNSKEY]
-    -> [ResourceRecord]
-    -> [(RD_RRSIG, TTL)]
-    -> Ranking
-    -> ContextT IO (RRset, ContextT IO ())
-verifyAndCache _ [] _ _ = return (rrsetEmpty, return ())
-verifyAndCache dnskeys rrs@(_ : _) sigs rank = do
-    now <- liftIO =<< asks currentSeconds_
-    let crrsError [] _ = return (rrsetEmpty, return ())
-        crrsError sortedRRs@(ResourceRecord{..} : _) _ = do
-            logLines Log.WARN $
-                "verifyAndCache: no caching RR set:" : map (("\t" ++) . show) rrs
-            return
-                (RRset rrname rrtype rrclass rrttl [DNS.rdata x | x <- sortedRRs] [], return ())
-    withVerifiedRRset now dnskeys rrs sigs crrsError $
-        \_sortedRRs dom typ cls minTTL rds sigrds ->
-            return
-                ( RRset dom typ cls minTTL rds sigrds
-                , cacheRRset rank dom typ cls minTTL rds sigrds
-                )
 
 {-# WARNING
     recoverRRset
@@ -1362,21 +1304,6 @@ lookupCacheEither logMark dom typ = do
         , maybe "miss" (\(_, rank) -> "hit: " ++ show rank) result
         ]
     return result
-
-cacheRRset
-    :: Ranking
-    -> Domain
-    -> TYPE
-    -> CLASS
-    -> TTL
-    -> [RData]
-    -> [RD_RRSIG]
-    -> ContextT IO ()
-cacheRRset rank dom typ cls ttl rds _sigrds = do
-    insertRRSet <- asks insert_
-    logLn Log.DEBUG $
-        "cacheRRset: " ++ show (((dom, typ, cls), ttl), rank) ++ "  " ++ show rds
-    liftIO $ insertRRSet (DNS.Question dom typ cls) ttl (Right rds) rank {- TODO: cache with RD_RRSIG -}
 
 cacheNoRRSIG :: [ResourceRecord] -> Ranking -> ContextT IO ()
 cacheNoRRSIG rrs0 rank = do
