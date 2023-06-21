5e6aa1b7c6f83c46057a1ea076b98bed14c9abc1
Kazu Yamamoto <kazu@iij.ad.jp>
2023-06-21 20:27:32 +0900

    extracting Delegation

---
+++ dnsext-full-resolver/DNS/Cache/Iterative/Delegation.hs
@@ -0,0 +1,162 @@
+module DNS.Cache.Iterative.Delegation (
+    lookupDelegation,
+    delegationWithCache,
+    MayDelegation (..),
+    mayDelegation,
+) where
+
+-- GHC packages
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Reader (asks)
+import Data.Function (on)
+import Data.List (groupBy, sort, uncons)
+import qualified Data.Set as Set
+
+-- other packages
+
+import System.Console.ANSI.Types
+
+-- dns packages
+
+import DNS.Do53.Memo (
+    rankedAdditional,
+    rankedAuthority,
+ )
+import DNS.SEC (
+    RD_DNSKEY,
+    TYPE (DS),
+ )
+import DNS.Types (
+    DNSMessage,
+    Domain,
+    ResourceRecord (..),
+    TYPE (A, AAAA, NS),
+ )
+import qualified DNS.Types as DNS
+import Data.IP (IP (IPv4, IPv6))
+
+-- this package
+import DNS.Cache.Iterative.Cache
+import DNS.Cache.Iterative.Helpers
+import DNS.Cache.Iterative.Types
+import DNS.Cache.Iterative.Utils
+import DNS.Cache.Iterative.Verify
+import qualified DNS.Log as Log
+
+-- If Nothing, it is a miss-hit against the cache.
+-- If Just NoDelegation, cache hit but no delegation information.
+lookupDelegation :: Domain -> ContextT IO (Maybe MayDelegation)
+lookupDelegation dom = do
+    disableV6NS <- asks disableV6NS_
+    let lookupDEs ns = do
+            let deListA =
+                    rrListWith
+                        A
+                        (`DNS.rdataField` DNS.a_ipv4)
+                        ns
+                        (\v4 _ -> DEwithAx ns (IPv4 v4))
+                deListAAAA =
+                    rrListWith
+                        AAAA
+                        (`DNS.rdataField` DNS.aaaa_ipv6)
+                        ns
+                        (\v6 _ -> DEwithAx ns (IPv6 v6))
+
+            lk4 <- fmap (deListA . fst) <$> lookupCache ns A
+            lk6 <- fmap (deListAAAA . fst) <$> lookupCache ns AAAA
+            return $ case lk4 <> lk6 of
+                Nothing
+                    | ns `DNS.isSubDomainOf` dom -> [] {- miss-hit with sub-domain case cause iterative loop, so return null to skip this NS -}
+                    | otherwise -> [DEonlyNS ns {- the case both A and AAAA are miss-hit -}]
+                Just as -> as {- just return address records. null case is wrong cache, so return null to skip this NS -}
+        noCachedV4NS es = disableV6NS && null (v4DEntryList es)
+        fromDEs es
+            | noCachedV4NS es = Nothing
+            {- all NS records for A are skipped under disableV6NS, so handle as miss-hit NS case -}
+            | otherwise =
+                (\des -> HasDelegation $ Delegation dom des [] []) <$> uncons es
+        {- Nothing case, all NS records are skipped, so handle as miss-hit NS case -}
+        getDelegation :: ([ResourceRecord], a) -> ContextT IO (Maybe MayDelegation)
+        getDelegation (rrs, _) = do
+            {- NS cache hit -}
+            let nss = sort $ nsList dom const rrs
+            case nss of
+                [] -> return $ Just NoDelegation {- hit null NS list, so no delegation -}
+                _ : _ -> fromDEs . concat <$> mapM lookupDEs nss
+
+    maybe (return Nothing) getDelegation =<< lookupCache dom NS
+
+v4DEntryList :: [DEntry] -> [DEntry]
+v4DEntryList [] = []
+v4DEntryList des@(de : _) = concatMap skipAAAA $ byNS des
+  where
+    byNS = groupBy ((==) `on` nsDomain)
+    skipAAAA = nullCase . filter (not . aaaaDE)
+      where
+        aaaaDE (DEwithAx _ (IPv6{})) = True
+        aaaaDE _ = False
+        nullCase [] = [DEonlyNS (nsDomain de)]
+        nullCase es@(_ : _) = es
+
+nsDomain :: DEntry -> Domain
+nsDomain (DEwithAx dom _) = dom
+nsDomain (DEonlyNS dom) = dom
+
+data MayDelegation
+    = NoDelegation {- no delegation information -}
+    | HasDelegation Delegation
+
+mayDelegation :: a -> (Delegation -> a) -> MayDelegation -> a
+mayDelegation n h md = case md of
+    NoDelegation -> n
+    HasDelegation d -> h d
+
+-- Caching while retrieving delegation information from the authoritative server's reply
+delegationWithCache
+    :: Domain -> [RD_DNSKEY] -> Domain -> DNSMessage -> DNSQuery MayDelegation
+delegationWithCache zoneDom dnskeys dom msg = do
+    (verifyMsg, verifyColor, raiseOnFailure, dss, cacheDS) <- withSection rankedAuthority msg $ \rrs rank -> do
+        let (dsrds, dsRRs) = unzip $ rrListWith DS DNS.fromRData dom (,) rrs
+        (rrset, cacheDS) <-
+            lift $ verifyAndCache dnskeys dsRRs (rrsigList dom DS rrs) rank
+        let (verifyMsg, verifyColor, raiseOnFailure)
+                | null nsps = ("no delegation", Nothing, pure ())
+                | null dsrds = ("delegation - no DS, so no verify", Just Yellow, pure ())
+                | rrsetVerified rrset =
+                    ("delegation - verification success - RRSIG of DS", Just Green, pure ())
+                | otherwise =
+                    ( "delegation - verification failed - RRSIG of DS"
+                    , Just Red
+                    , throwDnsError DNS.ServerFailure
+                    )
+        return
+            ( verifyMsg
+            , verifyColor
+            , raiseOnFailure
+            , if rrsetVerified rrset then dsrds else []
+            , cacheDS
+            )
+
+    let found x = do
+            cacheDS
+            cacheNS
+            cacheAdds
+            clogLn Log.DEMO Nothing $ ppDelegation (delegationNS x)
+            return x
+
+    lift . clogLn Log.DEMO verifyColor $
+        "delegationWithCache: " ++ domTraceMsg ++ ", " ++ verifyMsg
+    raiseOnFailure
+    lift . maybe (pure NoDelegation) (fmap HasDelegation . found) $
+        takeDelegationSrc nsps dss adds {- There is delegation information only when there is a selectable NS -}
+  where
+    domTraceMsg = show zoneDom ++ " -> " ++ show dom
+
+    (nsps, cacheNS) = withSection rankedAuthority msg $ \rrs rank ->
+        let nsps_ = nsList dom (,) rrs in (nsps_, cacheNoRRSIG (map snd nsps_) rank)
+
+    (adds, cacheAdds) = withSection rankedAdditional msg $ \rrs rank ->
+        let axs = filter match rrs in (axs, cacheSection axs rank)
+      where
+        match rr = rrtype rr `elem` [A, AAAA] && rrname rr `Set.member` nsSet
+        nsSet = Set.fromList $ map fst nsps
