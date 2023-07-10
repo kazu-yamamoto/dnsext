5b62970fd81bc42ef68a97596e53cbf81d0d77a5
Kazu Yamamoto <kazu@iij.ad.jp>
2023-07-10 12:28:32 +0900

    using http2-tls for TLS

--- dnsext-dox/DNS/DoX/TLS.hs
+++ dnsext-dox/DNS/DoX/TLS.hs
@@ -6,23 +6,16 @@ module DNS.DoX.TLS where
 import DNS.Do53.Internal
 import Data.ByteString.Char8 ()
 import qualified Network.HTTP2.TLS.Client as H2
-import qualified Network.HTTP2.TLS.Internal as H2
-import Network.Socket hiding (recvBuf)
 import Network.Socket.BufferPool (makeRecvN)
-import Network.TLS (bye, contextNew, handshake)
-import qualified UnliftIO.Exception as E
 
 tlsResolver :: VCLimit -> Resolver
 tlsResolver lim ri@ResolvInfo{..} q qctl = vcResolver "TLS" perform ri q qctl
   where
     -- Using a fresh connection
-    perform solve = E.bracket open close $ \sock -> do
-        E.bracket (contextNew sock params) bye $ \ctx -> do
-            handshake ctx
-            recvN <- makeRecvN "" $ H2.recvTLS ctx
-            let sendDoT = sendVC $ H2.sendManyTLS ctx
+    perform solve = H2.runTLS rinfoHostName rinfoPortNumber "dot" f
+      where
+        f H2.IOBackend{..} = do
+            recvN <- makeRecvN "" recv
+            let sendDoT = sendVC sendMany
                 recvDoT = recvVC lim recvN
             solve sendDoT recvDoT
-
-    open = openTCP rinfoHostName rinfoPortNumber
-    params = H2.getClientParams rinfoHostName "dot" False
