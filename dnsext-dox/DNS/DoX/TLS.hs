360ca0031da401f63c28eb59c6be8e6a008a39a8
Kazu Yamamoto <kazu@iij.ad.jp>
2023-07-13 12:04:07 +0900

    catching up new http2-tls

--- dnsext-dox/DNS/DoX/TLS.hs
+++ dnsext-dox/DNS/DoX/TLS.hs
@@ -6,16 +6,17 @@ module DNS.DoX.TLS where
 import DNS.Do53.Internal
 import Data.ByteString.Char8 ()
 import qualified Network.HTTP2.TLS.Client as H2
+import qualified Network.HTTP2.TLS.Internal as H2
 import Network.Socket.BufferPool (makeRecvN)
 
 tlsResolver :: VCLimit -> Resolver
 tlsResolver lim ri@ResolvInfo{..} q qctl = vcResolver "TLS" perform ri q qctl
   where
     -- Using a fresh connection
-    perform solve = H2.runTLS H2.defaultSettings rinfoHostName rinfoPortNumber "dot" solve'
+    perform solve = H2.runTLS rinfoHostName rinfoPortNumber "dot" solve'
       where
-        solve' _mgr backend = do
-            recvN <- makeRecvN "" $ H2.recv backend
-            let sendDoT = sendVC $ H2.sendMany backend
+        solve' ctx = do
+            recvN <- makeRecvN "" $ H2.recvTLS ctx
+            let sendDoT = sendVC $ H2.sendManyTLS ctx
                 recvDoT = recvVC lim recvN
             solve sendDoT recvDoT
