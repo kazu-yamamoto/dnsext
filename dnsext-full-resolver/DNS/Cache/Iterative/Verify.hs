e0ea72c7827d394f6e0b3761fe05210169152b3e
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-19 21:54:46 +0900

    drop old verify util

--- dnsext-full-resolver/DNS/Cache/Iterative/Verify.hs
+++ dnsext-full-resolver/DNS/Cache/Iterative/Verify.hs
@@ -4,7 +4,6 @@
 module DNS.Cache.Iterative.Verify (
     withCanonical,
     withCanonical',
-    verifyAndCache,
 ) where
 
 -- GHC packages
@@ -39,54 +38,6 @@ import DNS.Cache.Iterative.Helpers
 import DNS.Cache.Iterative.Types
 import DNS.Cache.Iterative.Utils
 import qualified DNS.Log as Log
-
-{-# DEPRECATED verifyAndCache "Use withCanonical or withCanonical'" #-}
-verifyAndCache
-    :: [RD_DNSKEY]
-    -> [ResourceRecord]
-    -> [(RD_RRSIG, TTL)]
-    -> Ranking
-    -> ContextT IO (RRset, ContextT IO ())
-verifyAndCache _ [] _ _ = return (rrsetEmpty, return ())
-verifyAndCache dnskeys rrs@(_ : _) sigs rank = do
-    now <- liftIO =<< asks currentSeconds_
-    let crrsError [] _ = return (rrsetEmpty, return ())
-        crrsError (_ : _) _ = do
-            logLines Log.WARN $ "verifyAndCache: no caching RR set:" : map (("\t" ++) . show) rrs
-            return (rrsetEmpty, return ())
-    withVerifiedRRset_ now dnskeys rrs sigs crrsError $
-        \_sortedRRs dom typ cls minTTL rds sigrds ->
-            return (RRset dom typ cls minTTL rds sigrds, cacheRRset rank dom typ cls minTTL rds sigrds)
-
-{- `left` is not RRset case. `right` is just RRset case.
-   `[RD_RRSIG]` is not null on verification success case. -}
-withVerifiedRRset_
-    :: EpochTime
-    -> [RD_DNSKEY]
-    -> [ResourceRecord]
-    -> [(RD_RRSIG, TTL)]
-    -> ([ResourceRecord] -> String -> a)
-    -> ([ResourceRecord] -> Domain -> TYPE -> CLASS -> TTL -> [RData] -> MayVerifiedRRS -> a)
-    -> a
-withVerifiedRRset_ now dnskeys rrs sigs left right =
-    either (left sortedRRs) ($ rightK) $ SEC.canonicalRRsetSorted sortedRRs
-  where
-    rightK dom typ cls ttl rds = right sortedRRs dom typ cls minTTL rds goodSigRDs
-      where
-        goodSigs =
-            [ rrsig
-            | rrsig@(sigrd, _) <- sigs
-            , key <- dnskeys
-            , Right () <- [SEC.verifyRRSIGsorted (toDNSTime now) key sigrd typ ttl sortedWires]
-            ]
-        (sigrds, sigTTLs) = unzip goodSigs
-        goodSigRDs
-            | null dnskeys = NotVerifiedRRS {- no way to verify  -}
-            | null sigs = InvalidRRS {- dnskeys is not null, but sigs is null -}
-            | otherwise = ValidRRS sigrds
-        expireTTLs = [exttl | sig <- sigrds, let exttl = fromDNSTime (rrsig_expiration sig) - now, exttl > 0]
-        minTTL = minimum $ ttl : sigTTLs ++ map fromIntegral expireTTLs
-    (sortedWires, sortedRRs) = unzip $ SEC.sortCanonical rrs
 
 withCanonical
     :: [RD_DNSKEY]
@@ -158,9 +109,6 @@ canonicalRRset rrs =
   where
     rightK dom typ cls ttl rds = (RRset dom typ cls ttl rds NotVerifiedRRS, sortedWires)
     (sortedWires, sortedRRs) = unzip $ SEC.sortCanonical rrs
-
-rrsetEmpty :: RRset
-rrsetEmpty = RRset "" (DNS.toTYPE 0) 0 0 [] NotVerifiedRRS
 
 cacheRRset
     :: Ranking
