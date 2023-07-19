935282dee4eab5464f7a8eaf002d35b77b8fdcf0
Kei Hibino <ex8k.hibino@gmail.com>
2023-07-19 21:10:48 +0900

    apply fix. resolv-just, query-ds

--- dnsext-full-resolver/DNS/Cache/Iterative/ResolveJust.hs
+++ dnsext-full-resolver/DNS/Cache/Iterative/ResolveJust.hs
@@ -417,12 +417,10 @@ queryDS
     -> DNSQuery (Either String [RD_DS], Color, String)
 queryDS dnskeys ips dom = do
     msg <- norec True ips dom DS
-    withSection rankedAnswer msg $ \rrs rank -> do
-        let (dsrds, dsRRs) = unzip $ rrListWith DS DNS.fromRData dom (,) rrs
-            rrsigs = rrsigList dom DS rrs
-        (rrset, cacheDS) <- lift $ verifyAndCache dnskeys dsRRs rrsigs rank
-        let verifyResult
-                | null dsrds = return (Right [], Yellow, "no DS, so no verify")
-                | rrsetValid rrset = lift cacheDS *> return (Right dsrds, Green, "verification success - RRSIG of DS")
-                | otherwise = return (Left "queryDS: verification failed - RRSIG of DS", Red, "verification failed - RRSIG of DS")
-        verifyResult
+    lift $ Verify.withCanonical dnskeys rankedAnswer msg dom DS (DNS.fromRData . rdata) nullDS ncDS verifyResult
+  where
+    nullDS = pure (Right [], Yellow, "no DS, so no verify")
+    ncDS = pure (Left "queryDS: not canonical DS", Red, "not canonical DS")
+    verifyResult dsrds dsRRset cacheDS
+        | rrsetValid dsRRset = cacheDS $> (Right dsrds, Green, "verification success - RRSIG of DS")
+        | otherwise = pure (Left "queryDS: verification failed - RRSIG of DS", Red, "verification failed - RRSIG of DS")
