20d71724fd4e7c33bf38817837c97aa039a4a453
Kazu Yamamoto <kazu@iij.ad.jp>
2023-08-25 15:13:07 +0900

    adding spec

---
+++ dnsext-dnstap/test/FastStreamSpec.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module FastStreamSpec where
+
+import DNS.TAP.FastStream
+import Data.ByteString ()
+import Data.IORef
+import Network.Run.TCP
+import Test.Hspec
+import UnliftIO.Concurrent
+import qualified UnliftIO.Exception as E
+
+spec :: Spec
+spec = do
+    describe "reader & writer" $ do
+        it "can send stream correctly in uni-directional" readWrite
+
+readWrite :: IO ()
+readWrite = do
+    mvar <- newEmptyMVar
+    E.bracket (forkIO $ server mvar ) killThread $ \_ -> client mvar
+  where
+    n = 10 :: Int
+    client mvar = runTCPClient "127.0.0.1" "50002" $ \sock -> do
+        ctx <- newContext sock $ Config False False False
+        ref <- newIORef 0
+        writer ctx $ do
+            i <- readIORef ref
+            if i < n
+                then do
+                  let i' = i + 1
+                  writeIORef ref i'
+                  return "foo!"
+                else return ""
+        takeMVar mvar `shouldReturn` ()
+    server mvar = runTCPServer (Just "127.0.0.1") "50002" $ \sock -> do
+        ctx <- newContext sock $ Config False True False
+        ref <- newIORef 0
+        reader ctx $ \_ -> modifyIORef' ref (+1)
+        readIORef ref `shouldReturn` n
+        putMVar mvar ()
+
