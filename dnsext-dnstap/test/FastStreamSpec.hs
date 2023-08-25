386bedf871c7e095388a6e080224493685b5bc48
Kazu Yamamoto <kazu@iij.ad.jp>
2023-08-25 15:29:55 +0900

    adding a test case for bidi

--- dnsext-dnstap/test/FastStreamSpec.hs
+++ dnsext-dnstap/test/FastStreamSpec.hs
@@ -13,28 +13,35 @@ import qualified UnliftIO.Exception as E
 spec :: Spec
 spec = do
     describe "reader & writer" $ do
-        it "can send stream correctly in uni-directional" readWrite
+        it "can send stream correctly in uni-directional" $ do
+            let conf = Config False False
+            readWrite conf
+        it "can send stream correctly in bi-directional" $ do
+            let conf = Config True False
+            readWrite conf
 
-readWrite :: IO ()
-readWrite = do
+readWrite :: Config -> IO ()
+readWrite conf = do
     mvar <- newEmptyMVar
     E.bracket (forkIO $ server mvar ) killThread $ \_ -> client mvar
   where
     n = 10 :: Int
-    client mvar = runTCPClient "127.0.0.1" "50002" $ \sock -> do
-        ctx <- newWriterContext sock $ Config False False
-        ref <- newIORef 0
-        writer ctx $ do
-            i <- readIORef ref
-            if i < n
-                then do
-                  let i' = i + 1
-                  writeIORef ref i'
-                  return "foo!"
-                else return ""
-        takeMVar mvar `shouldReturn` ()
+    client mvar = do
+        threadDelay 10000
+        runTCPClient "127.0.0.1" "50002" $ \sock -> do
+            ctx <- newWriterContext sock conf
+            ref <- newIORef 0
+            writer ctx $ do
+                i <- readIORef ref
+                if i < n
+                    then do
+                      let i' = i + 1
+                      writeIORef ref i'
+                      return "foo!"
+                    else return ""
+            takeMVar mvar `shouldReturn` ()
     server mvar = runTCPServer (Just "127.0.0.1") "50002" $ \sock -> do
-        ctx <- newReaderContext sock $ Config False False
+        ctx <- newReaderContext sock conf
         ref <- newIORef 0
         reader ctx $ \_ -> modifyIORef' ref (+1)
         readIORef ref `shouldReturn` n
