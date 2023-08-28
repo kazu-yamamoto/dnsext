54446eab0986f91aace3633df2170423c430eec3
Kazu Yamamoto <kazu@iij.ad.jp>
2023-08-28 16:06:36 +0900

    implementing i64

--- dnsext-dnstap/DNS/TAP/ProtocolBuffer.hs
+++ dnsext-dnstap/DNS/TAP/ProtocolBuffer.hs
@@ -14,8 +14,6 @@ module DNS.TAP.ProtocolBuffer (
 ) where
 
 import Data.Bits
-import qualified Data.ByteString.Base16 as B16
-import qualified Data.ByteString.Char8 as C8
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IM
 import Network.ByteOrder
@@ -122,35 +120,13 @@ i32 rbuf = do
     return ((n3 `shiftL` 24) .|. (n2 `shiftL` 16) .|. (n1 `shiftL` 8) .|. n0)
 
 i64 :: Readable a => a -> IO Int
-i64 = undefined
-
-----------------------------------------------------------------
-
-skip :: Readable a1 => a1 -> WireType -> IO ()
-skip rbuf VARINT = do
-    len <- varint rbuf
-    putStrLn $ "skipping VARINT " ++ show len
-skip rbuf I64 = do
-    _ <- read64 rbuf -- fixme endian
-    putStrLn "skipping I64"
-skip rbuf LEN = do
-    len <- varint rbuf
-    putStrLn $ "skipping LEN " ++ show len
-    ff rbuf len
-skip rbuf I32 = do
-    _ <- i32 rbuf
-    putStrLn "skipping I32"
-skip rbuf _ = do
-    putStrLn "skipping VARINT unknown"
-    remainingSize rbuf >>= ff rbuf
-
-dump :: Readable a => a -> IO ()
-dump rbuf = do
-    len <- varint rbuf
-    bs <- extractByteString rbuf len
-    C8.putStrLn $ B16.encode bs
-
-dumpASCII :: Readable a => a -> IO ()
-dumpASCII rbuf = do
-    len <- varint rbuf
-    extractByteString rbuf len >>= C8.putStrLn
+i64 rbuf = do
+    n0 <- fromIntegral <$> read8 rbuf
+    n1 <- fromIntegral <$> read8 rbuf
+    n2 <- fromIntegral <$> read8 rbuf
+    n3 <- fromIntegral <$> read8 rbuf
+    n4 <- fromIntegral <$> read8 rbuf
+    n5 <- fromIntegral <$> read8 rbuf
+    n6 <- fromIntegral <$> read8 rbuf
+    n7 <- fromIntegral <$> read8 rbuf
+    return ((n7 `shiftL` 56) .|. (n6 `shiftL` 48) .|. (n5 `shiftL` 40) .|. (n4 `shiftL` 32) .|. (n3 `shiftL` 24) .|. (n2 `shiftL` 16) .|. (n1 `shiftL` 8) .|. n0)
