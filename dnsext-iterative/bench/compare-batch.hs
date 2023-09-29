9f2b13c3a9cdc0f26adfd64b9dce31455efbaf31
Kei Hibino <ex8k.hibino@gmail.com>
2023-09-29 14:00:22 +0900

    compare-batch: catch-up to updated libraries

--- dnsext-iterative/bench/compare-batch.hs
+++ dnsext-iterative/bench/compare-batch.hs
@@ -1,32 +1,43 @@
--- ghc packages
+-- GHC packages
 import Control.Concurrent (threadDelay)
+import qualified Control.Exception as E
 import Control.Monad ((<=<))
-import qualified Data.ByteString.Char8 as B8
 import Data.Char (toUpper)
 import Data.Function (on)
-import Data.Int (Int64)
-import Data.List (intercalate, sort)
-import Data.Time.Clock.System (getSystemTime, systemSeconds)
+import Data.List (intercalate, sort, sortOn)
+import Data.String (fromString)
+import Data.UnixTime (UnixTime (..), getUnixTime)
 import Data.Word (Word16)
+import Foreign.C.Types (CTime (..))
 import System.Environment (getArgs)
 import Text.Read (readEither)
 
--- dns packages
-import Network.DNS (
+-- dnsext-* package
+
+import DNS.Do53.Client (
+    FlagOp (..),
+    defaultResolvActions,
+    ractionGenId,
+    ractionGetTime,
+    ractionLog,
+ )
+import qualified DNS.Do53.Client as DNS
+import DNS.Do53.Internal (
+    ResolvEnv (..),
+    ResolvInfo (..),
+    defaultResolvInfo,
+    udpTcpResolver,
+ )
+import qualified DNS.Do53.Internal as DNS
+import DNS.Types (
+    CLASS (IN),
     DNSError,
     DNSMessage,
     Domain,
-    ResolvConf (..),
-    ResolvSeed,
     ResourceRecord,
     TYPE (..),
  )
-import qualified Network.DNS as DNS
-
--- thi package
-
-import DNS.Iterative.Cache (lowerAnswer, lowerAuthority)
-import DNS.Iterative.DNSUtil (lookupRaw)
+import qualified DNS.Types as DNS
 
 run
     :: (String, Word16)
@@ -34,16 +45,16 @@ run
     -> [String]
     -> IO ()
 run (h1, p1) (h2, p2) inputs = do
-    rs1 <- mkRS h1 p1
-    rs2 <- mkRS h2 p2
-    let step = putStrLn <=< either (return . skip) (uncurry $ doCompare rs1 rs2) . getParam
+    re1 <- mkRE h1 p1
+    re2 <- mkRE h2 p2
+    let step = putStrLn <=< either (return . skip) (uncurry $ doCompare re1 re2) . getParam
     mapM_ step inputs
   where
     skip xs = intercalate "\t" $ "3" : xs
     getParam s = case words s of
         (h : tn : _) -> do
             typ <- readTYPE s tn
-            Right (B8.pack h, typ)
+            Right (fromString h, typ)
         _ -> Left [s, "run: unknown input form"]
     readTYPE s tn = case map toUpper tn of
         "A" -> Right A
@@ -55,28 +66,41 @@ run (h1, p1) (h2, p2) inputs = do
         "TXT" -> Right TXT
         _ -> Left [s, "run: unsupported type: " ++ tn]
 
-mkRS :: String -> Word16 -> IO ResolvSeed
-mkRS host port =
-    DNS.makeResolvSeed
-        DNS.defaultResolvConf
-            { resolvInfo = DNS.RCHostPort host (fromIntegral port)
-            , resolvTimeout = 6 * 1000 * 1000
-            , resolvRetry = 1
+mkRE :: String -> Word16 -> IO ResolvEnv
+mkRE host port = renv <$> DNS.newConcurrentGenId
+  where
+    ris genId =
+        [ defaultResolvInfo
+            { rinfoHostName = host
+            , rinfoPortNumber = fromIntegral port
+            , rinfoActions =
+                defaultResolvActions
+                    { ractionGenId = genId
+                    , ractionGetTime = getUnixTime >>= \(UnixTime (CTime now) _) -> pure now
+                    , ractionLog = \_ _ _ -> pure ()
+                    }
+            }
+        ]
+    renv genId =
+        ResolvEnv
+            { renvResolver = udpTcpResolver 3 (32 * 1024) -- 3 is retry
+            , renvConcurrent = False
+            , renvResolvInfos = ris genId
             }
 
-doCompare :: ResolvSeed -> ResolvSeed -> Domain -> TYPE -> IO String
-doCompare rs1 rs2 name typ = do
-    e1 <- queries rs1 name typ
-    e2 <- queries rs2 name typ
+doCompare :: ResolvEnv -> ResolvEnv -> Domain -> TYPE -> IO String
+doCompare re1 re2 name typ = do
+    e1 <- queries re1 name typ
+    e2 <- queries re2 name typ
     let (code, note) = compareResult e1 e2
-    return $ intercalate "\t" [show code, B8.unpack name, show typ, note]
+    return $ intercalate "\t" [show code, DNS.toRepresentation name, show typ, note]
 
 queries
-    :: ResolvSeed
+    :: ResolvEnv
     -> Domain
     -> TYPE
     -> IO (Either DNSError DNSMessage)
-queries rs name typ = recurse (3 :: Int)
+queries re name typ = recurse (1 :: Int)
   where
     recurse n
         | n <= 0 = fail "queries.recurse: positive integer required."
@@ -89,18 +113,18 @@ queries rs name typ = recurse (3 :: Int)
                     | otherwise = pure $ Right msg
             either dnsError dnsMessage =<< query1
 
-    query1 = do
-        now <- systemSeconds <$> getSystemTime
-        query now rs name typ
+    query1 = query re name typ
     rcode = DNS.rcode . DNS.flags . DNS.header
 
 query
-    :: Int64
-    -> ResolvSeed
+    :: ResolvEnv
     -> Domain
     -> TYPE
     -> IO (Either DNSError DNSMessage)
-query now rs name typ = DNS.withResolver rs $ \resolver -> lookupRaw now resolver name typ
+query re name typ =
+    E.try (getMsg <$> DNS.resolve re (DNS.Question name typ IN) (DNS.rdFlag FlagSet))
+  where
+    getMsg (DNS.Result{DNS.resultReply = DNS.Reply{DNS.replyDNSMessage = msg}}) = msg
 
 type Result = Either DNSError DNSMessage
 
@@ -115,7 +139,7 @@ compareResult x1 x2 = case (x1, x2) of
         | e1 == e2 = (0, "OK, same dns-errors")
         | otherwise = (1, "dns-error not match: " ++ show (e1, e2))
     compareMessage m1 m2
-        | qr m1 /= qr m2 = (1, "qOrR not match: " ++ show (qr m1, qr m2))
+        | not (isResp m1) || not (isResp m2) = (1, "isResponse not match: " ++ show (isResp m1, isResp m2))
         | rcode m1 /= rcode m2 = (1, "rcode not match: " ++ show (rcode m1, rcode m2))
         | ra m1 /= ra m2 =
             (1, "recAvailable not match: " ++ show (ra m1, ra m2))
@@ -123,34 +147,34 @@ compareResult x1 x2 = case (x1, x2) of
         | neqAnswer1 m1 m2 =
             ( 1
             , "answer types not match: "
-                ++ show (lowerAnswer m1)
+                ++ show (DNS.answer m1)
                 ++ ", "
-                ++ show (lowerAnswer m2)
+                ++ show (DNS.answer m2)
             )
         | neqAnswer2 m1 m2 =
             ( 2
             , "answer data not match: "
-                ++ show (lowerAnswer m1)
+                ++ show m1
                 ++ ", "
-                ++ show (lowerAnswer m2)
+                ++ show (DNS.answer m2)
             )
         | neqAuthority2 m1 m2 =
             ( 2
             , "authority data not match: "
-                ++ show (lowerAuthority m1)
+                ++ show (DNS.authority m1)
                 ++ ", "
-                ++ show (lowerAuthority m2)
+                ++ show (DNS.authority m2)
             )
         | otherwise = (0, "OK, replies match")
 
-    qr = DNS.qOrR . flags
+    isResp = DNS.isResponse . flags
     rcode = DNS.rcode . flags
     ra = DNS.recAvailable . flags
     rd = DNS.recDesired . flags
     flags = DNS.flags . DNS.header
-    neqAnswer1 m1 m2 = not $ (eqSection1 `on` lowerAnswer) m1 m2
-    neqAnswer2 m1 m2 = not $ (eqSection2 `on` lowerAnswer) m1 m2
-    neqAuthority2 m1 m2 = not $ (eqSection2 `on` lowerAuthority) m1 m2
+    neqAnswer1 m1 m2 = not $ (eqSection1 `on` DNS.answer) m1 m2
+    neqAnswer2 m1 m2 = not $ (eqSection2 `on` DNS.answer) m1 m2
+    neqAuthority2 m1 m2 = not $ (eqSection2 `on` DNS.authority) m1 m2
 
 eqSection2 :: [ResourceRecord] -> [ResourceRecord] -> Bool
 eqSection2 s1 s2 =
@@ -158,7 +182,7 @@ eqSection2 s1 s2 =
         && all (uncurry (==)) (zip (reg s1) (reg s2))
   where
     fields = (,,,) <$> DNS.rrname <*> DNS.rrtype <*> DNS.rrclass <*> DNS.rdata
-    reg = sort . map fields
+    reg = sortOn (\(n, t, c, _) -> (n, t, c)) . map fields
 
 -- only check name, type, class
 eqSection1 :: [ResourceRecord] -> [ResourceRecord] -> Bool
