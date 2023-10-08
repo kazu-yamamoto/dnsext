e4ff176fc008a9d672fb7f581387299c9d8cc1cc
Kazu Yamamoto <kazu@iij.ad.jp>
2023-10-08 13:09:41 +0900

    adding missing module

---
+++ dnsext-types/DNS/Wire.hs
@@ -0,0 +1,9 @@
+module DNS.Wire (
+    module DNS.Wire.Builder,
+    module DNS.Wire.Parser,
+    module DNS.Wire.Types,
+) where
+
+import DNS.Wire.Builder
+import DNS.Wire.Parser
+import DNS.Wire.Types
