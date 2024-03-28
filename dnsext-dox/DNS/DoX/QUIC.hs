1e8c602335c8fdc895a56f995345bb39573c6ff0
Kazu Yamamoto <kazu@iij.ad.jp>
2024-03-28 18:02:00 +0900

    fixing PersistentResolver or DoQ

--- dnsext-dox/DNS/DoX/QUIC.hs
+++ dnsext-dox/DNS/DoX/QUIC.hs
@@ -4,6 +4,8 @@
 module DNS.DoX.QUIC where
 
 import DNS.Do53.Internal
+import DNS.Types.Decode
+import qualified Data.ByteString as BS
 import Network.QUIC
 import Network.QUIC.Client
 import Network.QUIC.Internal hiding (Recv, shared)
@@ -13,25 +15,33 @@ import DNS.DoX.Imports
 
 quicPersistentResolver :: PersistentResolver
 quicPersistentResolver ri@ResolveInfo{..} body = run cc $ \conn -> do
-    strm <- stream conn
-    let sendDoQ bs = do
-            sendVC (sendStreamMany strm) bs
-            shutdownStream strm
-        recvDoQ = recvVC rinfoVCLimit $ recvStream strm
-    vcPersistentResolver "QUIC" sendDoQ recvDoQ ri body
+    body $ resolv conn ri
   where
     cc = getQUICParams rinfoIP rinfoPort "doq"
 
 quicResolver :: OneshotResolver
 quicResolver ri@ResolveInfo{..} q qctl = run cc $ \conn -> do
-    strm <- stream conn
-    let sendDoQ bs = do
-            sendVC (sendStreamMany strm) bs
-            shutdownStream strm
-        recvDoQ = recvVC rinfoVCLimit $ recvStream strm
-    vcResolver "QUIC" sendDoQ recvDoQ ri q qctl
+    resolv conn ri q qctl
   where
     cc = getQUICParams rinfoIP rinfoPort "doq"
+
+resolv :: Connection -> ResolveInfo -> Resolver
+resolv conn ri@ResolveInfo{..} q qctl = do
+    strm <- stream conn
+    ident <- ractionGenId rinfoActions
+    let qry = encodeQuery ident q qctl
+        tx = BS.length qry
+    sendVC (sendStreamMany strm) qry
+    shutdownStream strm
+    (rx, bss) <- recvVC rinfoVCLimit $ recvStream strm
+    now <- getTime
+    case decodeChunks now bss of
+        Left e -> return $ Left e
+        Right msg -> case checkRespM q ident msg of -- fixme
+            Nothing -> return $ Right $ toResult ri "doq" $ Reply msg tx rx
+            Just err -> return $ Left err
+  where
+    getTime = ractionGetTime rinfoActions
 
 getQUICParams :: IP -> PortNumber -> ByteString -> ClientConfig
 getQUICParams addr port alpn =
