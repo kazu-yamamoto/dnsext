cd2979e03f922c741b3a0ce5d318969c3c782b92
simamura@iij.ad.jp <Mitsuru Shimamura>
2024-06-04 17:27:12 +0900

    use tini for PID1 problem, improve CMD handling

--- build-docker/scripts/bowline.sh
+++ build-docker/scripts/bowline.sh
@@ -1,3 +1,16 @@
 #! /bin/sh
 
-/opt/bowline/bin/bowline /opt/bowline/etc/bowline.conf "$@"
+case "$1" in
+	sh|bash)
+		exe="$1"
+		shift
+		exec /bin/$exe "$@"
+		;;
+	dug)
+		shift
+		exec /opt/bowline/bin/dug "$@"
+		;;
+	*)
+		exec /opt/bowline/bin/bowline /opt/bowline/etc/bowline.conf "$@"
+		;;
+esac
