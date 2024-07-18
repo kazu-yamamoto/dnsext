08b6b573509ec00eadfc6e96cf2c32c84676f284
Kei Hibino <ex8k.hibino@gmail.com>
2024-07-18 15:32:19 +0900

    drop unused def

--- dnsext-bowline/bowline/SocketUtil.hs
+++ dnsext-bowline/bowline/SocketUtil.hs
@@ -1,21 +1,12 @@
 module SocketUtil (
-    addrInfo,
     ainfosSkipError,
     foldAddrInfo,
-    mkSocketWaitForByte,
-    isAnySockAddr,
 ) where
 
--- GHC internal packages
-import GHC.IO.Device (IODevice (ready))
-import GHC.IO.FD (mkFD)
-
+-- GHC packages
 import Data.Functor
 import Data.List
 import Data.Maybe
-
--- GHC packages
-import System.IO (IOMode (ReadMode))
 import System.IO.Error (tryIOError)
 
 -- dns packages
@@ -24,17 +15,10 @@ import Network.Socket (
     AddrInfoFlag (..),
     HostName,
     PortNumber,
-    SockAddr (..),
-    Socket,
     SocketType (..),
     defaultHints,
  )
 import qualified Network.Socket as S
-
-addrInfo :: PortNumber -> [HostName] -> IO [AddrInfo]
-addrInfo p [] = S.getAddrInfo Nothing Nothing $ Just $ show p
-addrInfo p hs@(_ : _) =
-    concat <$> sequence [S.getAddrInfo Nothing (Just h) $ Just $ show p | h <- hs]
 
 {- FOURMOLU_DISABLE -}
 ainfosSkipError :: (String -> IO ()) -> SocketType -> PortNumber -> [HostName] -> IO [AddrInfo]
@@ -62,40 +46,3 @@ foldAddrInfo left right socktype mhost port =
         | addrSocketType ai == socktype  = pure $ Just ai
         | otherwise                      = pure Nothing
 {- FOURMOLU_ENABLE -}
-
-{- make action to wait for socket-input from cached FD
-   without calling fdStat and mkFD for every wait-for calls -}
-mkSocketWaitForByte :: Socket -> IO (Int -> IO Bool)
-mkSocketWaitForByte sock =
-    withFD <$> S.withFdSocket sock getFD
-  where
-    withFD fd millisec =
-        ready fd False millisec
-    getFD fd =
-        fst
-            <$> mkFD
-                fd
-                ReadMode
-                Nothing {- stat, filled in `mkFD`, calling `fdStat` -}
-                False {- socket flag for only Windows -}
-                False {- non-blocking, False -}
-                {-
-                mkSocketWaitForInput sock =
-                  withStat <$> withFdSocket sock fdStat
-                  where
-                    withStat stat millisec = do
-                      (fd, _) <- withFdSocket sock $ getFD stat
-                      ready fd False millisec
-                    getFD stat fd =
-                      mkFD fd ReadMode
-                      (Just stat)  {- stat, get from `fdStat` -}
-                      False        {- socket flag for only Windows -}
-                      False        {- non-blocking, False -}
-
-                -- import System.Posix.Internals (fdStat)
-                -}
-
-isAnySockAddr :: SockAddr -> Bool
-isAnySockAddr (SockAddrInet _ 0) = True
-isAnySockAddr (SockAddrInet6 _ _ (0, 0, 0, 0) _) = True
-isAnySockAddr _ = False
