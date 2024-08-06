177fdf16aec55fcd9b17fec34ecc31739a0c21c7
Kei Hibino <ex8k.hibino@gmail.com>
2024-08-06 14:44:20 +0900

    time-cache: switch to no-thread AutoUpdate

--- dnsext-utils/DNS/TimeCache.hs
+++ dnsext-utils/DNS/TimeCache.hs
@@ -13,56 +13,39 @@ import qualified Data.ByteString.Char8 as C8
 import Foreign.C.Types (CTime (..))
 
 -- other packages
-import Control.AutoUpdate (
-    defaultUpdateSettings,
-    mkAutoUpdate,
-    updateAction,
-    updateFreq,
- )
 import Data.UnixTime (UnixTime (..), formatUnixTime, getUnixTime)
 
--- dnsext packages
+-- dnsext-* packages
 import DNS.Types.Time (EpochTime)
 
 -- this package
+import DNS.Utils.AutoUpdate (mkClosableAutoUpdate)
 
 {- FOURMOLU_DISABLE -}
 data TimeCache = TimeCache
-    { getTimestamp  :: IO UnixTime
-    , getTimeStr    :: IO ShowS
+    { getTimestamp    :: IO UnixTime
+    , getTimeStr      :: IO ShowS
+    , closeTimeCache  :: IO ()
     }
 {- FOURMOLU_ENABLE -}
 
+{- FOURMOLU_DISABLE -}
 newTimeCache :: Int -> IO TimeCache
 newTimeCache micros = do
-    getUTime <- mkAutoTimestamp micros getUnixTime
-    TimeCache getUTime <$> mkAutoTimeShowS getUTime
+    (onceGetTime  , close1) <- mkClosableAutoUpdate micros     getUnixTime
+    (onceGetString, close2) <- mkClosableAutoUpdate 1_000_000 (getTimeShowS =<< onceGetTime)
+    return $ TimeCache onceGetTime onceGetString (close2 >> close1)
+{- FOURMOLU_ENABLE -}
 
 getTime :: TimeCache -> IO EpochTime
 getTime = fmap unixToEpoch . getTimestamp
-
-mkAutoTimestamp :: Int -> IO UnixTime -> IO (IO UnixTime)
-mkAutoTimestamp micros getUTime = mostOncePerMicros micros getUTime
-
-mkAutoTimeShowS :: IO UnixTime -> IO (IO ShowS)
-mkAutoTimeShowS getUTime = mostOncePerSecond $ getTimeShowS =<< getUTime
-
-mostOncePerMicros :: Int -> IO a -> IO (IO a)
-mostOncePerMicros interval upd =
-    mkAutoUpdate
-        defaultUpdateSettings
-            { updateAction = upd
-            , updateFreq = interval
-            }
-
-mostOncePerSecond :: IO a -> IO (IO a)
-mostOncePerSecond = mostOncePerMicros 1_000_000
 
 noneTimeCache :: TimeCache
 noneTimeCache =
     TimeCache
         { getTimestamp = getUnixTime
         , getTimeStr = getTimeShowS =<< getUnixTime
+        , closeTimeCache = pure ()
         }
 
 ---
