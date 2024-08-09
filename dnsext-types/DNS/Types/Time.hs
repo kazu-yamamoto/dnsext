d2b677840f435a060af91819b0a186304194f2fa
Kei Hibino <ex8k.hibino@gmail.com>
2024-08-09 20:19:03 +0900

    dnsext-types: extend time interface for DNS

--- dnsext-types/DNS/Types/Time.hs
+++ dnsext-types/DNS/Types/Time.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module DNS.Types.Time (
     EpochTime,
+    EpochTimeUsec,
     getCurrentTime,
+    getCurrentTimeUsec,
     getCurrentTimeNsec,
+    epochUsecToSeconds,
+    diffMicroSec,
 ) where
 
 import Data.Int (Int64)
@@ -14,6 +20,19 @@ getCurrentTime :: IO EpochTime
 getCurrentTime = do
     UnixTime (CTime tim) _ <- getUnixTime
     return tim
+
+type EpochTimeUsec = UnixTime
+
+getCurrentTimeUsec :: IO EpochTimeUsec
+getCurrentTimeUsec = getUnixTime
+
+epochUsecToSeconds :: EpochTimeUsec -> EpochTime
+epochUsecToSeconds (UnixTime (CTime tim) _) =  tim
+
+diffMicroSec :: EpochTimeUsec -> EpochTimeUsec -> Int64
+diffMicroSec x y = toMicro $ diffUnixTime x y
+  where
+    toMicro (UnixDiffTime (CTime sec) u) = sec * 1_000_000 + fromIntegral u
 
 getCurrentTimeNsec :: IO (EpochTime, Int64)
 getCurrentTimeNsec = do
