bfa4e3363ce91782cf1a9e26592dab2c28003263
Kei Hibino <ex8k.hibino@gmail.com>
2024-10-15 18:36:53 +0900

    simplify buffer state for nbRecvN

--- dnsext-iterative/DNS/Iterative/Server/NonBlocking.hs
+++ dnsext-iterative/DNS/Iterative/Server/NonBlocking.hs
@@ -28,7 +28,7 @@ data NBRecvR = EOF ByteString | NotEnough | NBytes ByteString
 type NBRecv = IO NBRecvR
 type NBRecvN = Int -> IO NBRecvR
 
-data State = E | S ByteString | M ([ByteString] -> [ByteString])
+type Buffer = [ByteString] -> [ByteString]
 
 makeNBRecvVC :: VCLimit -> Recv -> IO NBRecv
 makeNBRecvVC lim rcv = do
@@ -37,8 +37,8 @@ makeNBRecvVC lim rcv = do
     return $ nbRecvVC lim ref nbrecvN
 
 makeNBRecvN :: Recv -> ByteString -> IO NBRecvN
-makeNBRecvN rcv "" = nbRecvN rcv <$> newIORef (0, E)
-makeNBRecvN rcv bs0 = nbRecvN rcv <$> newIORef (len, S bs0)
+makeNBRecvN rcv "" = nbRecvN rcv <$> newIORef (0, id)
+makeNBRecvN rcv bs0 = nbRecvN rcv <$> newIORef (len, (bs0 :))
   where
     len = BS.length bs0
 
@@ -74,59 +74,36 @@ nbRecvVC lim ref nbrecvN = do
 
 nbRecvN
     :: Recv
-    -> IORef (Int, State)
+    -> IORef (Int, Buffer)
     -> NBRecvN
 nbRecvN rcv ref n = do
-    (len0, st) <- readIORef ref
+    (len0, build0) <- readIORef ref
     if
         | len0 == n -> do
-            writeIORef ref (0, E)
-            case st of
-                E -> return $ NBytes ""
-                S bs0 -> return $ NBytes bs0
-                M build0 -> return $ NBytes $ BS.concat $ build0 []
+            writeIORef ref (0, id)
+            return $ NBytes $ BS.concat $ build0 []
         | len0 > n -> do
-            case st of
-                E -> error "nbRecvN E"
-                S bs0 -> do
-                    let (ret, left) = BS.splitAt n bs0
-                    writeIORef ref (BS.length left, S left)
-                    return $ NBytes ret
-                M build0 -> do
-                    -- slow path
-                    let bs = BS.concat $ build0 []
-                        (ret, left) = BS.splitAt n bs
-                    writeIORef ref (BS.length left, S left)
-                    return $ NBytes ret
+            let bs = BS.concat $ build0 []
+                (ret, left) = BS.splitAt n bs
+            writeIORef ref (BS.length left, (left :))
+            return $ NBytes ret
         | otherwise -> do
             bs1 <- rcv
             if BS.null bs1
                 then do
-                    writeIORef ref (0, E)
-                    case st of
-                        E -> return $ EOF ""
-                        S bs -> return $ EOF bs
-                        M build -> return $ EOF $ BS.concat $ build []
+                    writeIORef ref (0, id)
+                    return $ EOF $ BS.concat $ build0 []
                 else do
                     let len1 = BS.length bs1
                         len2 = len0 + len1
                     if
                         | len2 == n -> do
-                            writeIORef ref (0, E)
-                            case st of
-                                E -> return $ NBytes bs1
-                                S bs0 -> return $ NBytes (bs0 <> bs1)
-                                M build0 -> return $ NBytes $ BS.concat $ build0 [bs1]
+                            writeIORef ref (0, id)
+                            return $ NBytes $ BS.concat $ build0 [bs1]
                         | len2 > n -> do
                             let (bs3, left) = BS.splitAt (n - len0) bs1
-                            writeIORef ref (BS.length left, S left)
-                            case st of
-                                E -> return $ NBytes bs3
-                                S bs0 -> return $ NBytes (bs0 <> bs3)
-                                M build0 -> return $ NBytes $ BS.concat $ build0 [bs3]
+                            writeIORef ref (BS.length left, (left :))
+                            return $ NBytes $ BS.concat $ build0 [bs3]
                         | otherwise -> do
-                            case st of
-                                E -> writeIORef ref (len2, S bs1)
-                                S bs0 -> writeIORef ref (len2, M ((bs0 :) . (bs1 :)))
-                                M build0 -> writeIORef ref (len2, M (build0 . (bs1 :)))
+                            writeIORef ref (len2, build0 . (bs1 :))
                             return NotEnough
