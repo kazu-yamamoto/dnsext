e7ef5684b09e56b5ba79d189aeca89ec7e79cd1f
Kei Hibino <ex8k.hibino@gmail.com>
2024-12-01 19:19:09 +0900

    bowline: apply cache-failure-rcode-ttl

--- dnsext-bowline/bowline/bowline.hs
+++ dnsext-bowline/bowline/bowline.hs
@@ -125,6 +125,7 @@ runConfig tcache mcache mng0 conf@Config{..} = do
                 , localZones_ = localZones
                 , stubZones_ = stubZones
                 , maxNegativeTTL_ = cropMaxNegativeTTL cnf_cache_max_negative_ttl
+                , failureRcodeTTL_ = cropFailureRcodeTTL cnf_cache_failure_rcode_ttl
                 , updateHistogram_ = updateHistogram
                 , timeout_ = tmout
                 }
