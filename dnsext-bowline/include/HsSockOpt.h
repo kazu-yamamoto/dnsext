6f82f4d4c323c4eac26715c0eb1b4a4cf33fbbbc
Kei Hibino <ex8k.hibino@gmail.com>
2024-12-12 18:56:37 +0900

    additional TCP sockopt: add template macros, and sockopt data

---
+++ dnsext-bowline/include/HsSockOpt.h
@@ -0,0 +1,14 @@
+
+#ifndef HSSOCKOPT_H
+#define HSSOCKOPT_H
+
+#include "HsSockOptConfig.h"
+
+#ifdef HAVE_NETINET_IN_H
+# include <netinet/in.h>
+#endif
+#ifdef HAVE_NETINET_TCP_H
+# include <netinet/tcp.h>
+#endif
+
+#endif
