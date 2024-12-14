44a8f131ea7cbe318ac5fbead2cc7fec82c09fa5
Kei Hibino <ex8k.hibino@gmail.com>
2024-12-14 23:16:42 +0900

    monitor: show param and print

--- dnsext-bowline/bowline/Monitor.hs
+++ dnsext-bowline/bowline/Monitor.hs
@@ -148,7 +148,7 @@ console conf env Control{cacheControl=CacheControl{..},..} srvInfo inH outH ainf
             hPutStr outH "monitor> " *> hFlush outH
             either (const $ return ()) (\exit -> unless exit repl) =<< withWait waitQuit (handle (($> False) . print) step)
 
-    showParam outLn conf srvInfo
+    mapM_ outLn =<< getShowParam'
     repl
   where
     handle :: (SomeException -> IO a) -> IO a -> IO a
@@ -185,13 +185,14 @@ console conf env Control{cacheControl=CacheControl{..},..} srvInfo inH outH ainf
         "help" : [] -> Just $ Help Nothing
         _ -> Nothing
 
+    getShowParam' = getShowParam conf srvInfo
     outLn = hPutStrLn outH
 
     runCmd Quit = quitServer $> True
     runCmd Exit = return True
     runCmd cmd = dispatch cmd $> False
       where
-        dispatch  Param = showParam outLn conf srvInfo
+        dispatch  Param = mapM_ outLn =<< getShowParam'
         dispatch  Noop = return ()
         dispatch (Find ws) = do
             now <- currentSeconds_ env
@@ -253,12 +254,19 @@ withWait qstm blockAct =
 socketWaitRead :: Socket -> IO ()
 socketWaitRead sock = S.withFdSocket sock $ threadWaitRead . fromIntegral
 
-showParam :: (String -> IO ()) -> Config -> [String] -> IO ()
-showParam outLn conf srvInfo = do
-    outLn "---------- configs  ----------"
-    mapM_ outLn $ showConfig conf
-    outLn "---------- runtime  ----------"
-    outLn . ("capabilities: " ++) . show =<< getNumCapabilities
-    mapM_ outLn srvInfo
-    outLn . ("euid: " ++) . show =<< getEffectiveUserID
-    outLn . ("egid: " ++) . show =<< getEffectiveGroupID
+{- FOURMOLU_DISABLE -}
+getShowParam :: Config -> [String] -> IO [String]
+getShowParam conf srvInfo =
+    format <$> sequence
+        [ ("capabilities: " ++) . show <$> getNumCapabilities
+        , ("euid: " ++) . show <$> getEffectiveUserID
+        , ("egid: " ++) . show <$> getEffectiveGroupID
+        ]
+  where
+    format rtInfo =
+        [ "---------- configs  ----------" ] ++
+        showConfig conf                      ++
+        [ "---------- runtime  ----------" ] ++
+        srvInfo                              ++
+        rtInfo
+{- FOURMOLU_ENABLE -}
