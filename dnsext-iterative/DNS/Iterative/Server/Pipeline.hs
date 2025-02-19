562fb211704ae31f75150ff3523ae764b19afc63
Kei Hibino <ex8k.hibino@gmail.com>
2025-02-19 16:55:34 +0900

    pipeline: apply defined synonyms

--- dnsext-iterative/DNS/Iterative/Server/Pipeline.hs
+++ dnsext-iterative/DNS/Iterative/Server/Pipeline.hs
@@ -248,7 +248,7 @@ receiverVC
     :: String
     -> Env
     -> VcSession
-    -> RecvPI
+    -> IO (BS, Peer)
     -> (ToCacher -> IO ())
     -> MkInput
     -> IO VcFinished
@@ -310,12 +310,12 @@ receiverVCnonBlocking name env vcs@VcSession{..} peerInfo recv onRecv toCacher m
             toCacher $ mkInput_ bs peerInfo (VcPendingOp{vpReqNum = i, vpDelete = delPending}) ts
 
 receiverLogic
-    :: Env -> SockAddr -> RecvPI -> (ToCacher -> IO ()) -> (ToSender -> IO ()) -> SocketProtocol -> IO ()
+    :: Env -> SockAddr -> IO (BS, Peer) -> (ToCacher -> IO ()) -> (ToSender -> IO ()) -> SocketProtocol -> IO ()
 receiverLogic env mysa recv toCacher toSender proto =
     handledLoop env "receiverUDP" $ void $ receiverLogic' env mysa recv toCacher toSender proto
 
 receiverLogic'
-    :: Env -> SockAddr -> RecvPI -> (ToCacher -> IO ()) -> (ToSender -> IO ()) -> SocketProtocol -> IO Bool
+    :: Env -> SockAddr -> IO (BS, Peer) -> (ToCacher -> IO ()) -> (ToSender -> IO ()) -> SocketProtocol -> IO Bool
 receiverLogic' env mysa recv toCacher toSender proto = do
     (bs, peerInfo) <- recv
     ts <- currentTimeUsec_ env
@@ -328,7 +328,7 @@ receiverLogic' env mysa recv toCacher toSender proto = do
 noPendingOp :: VcPendingOp
 noPendingOp = VcPendingOp{vpReqNum = 0, vpDelete = pure ()}
 
-getSendVC :: VcTimer -> Send -> Send
+getSendVC :: VcTimer -> (BS -> Peer -> IO ()) -> BS -> Peer -> IO ()
 getSendVC timer send bs peerInfo = resetVcTimer timer >> send bs peerInfo
 
 senderVC
@@ -350,11 +350,11 @@ senderVC name env vcs send fromX = loop `E.catch` onError
     step = E.bracket fromX finalize $ \(Output bs _ peerInfo) -> send bs peerInfo
     finalize (Output _ VcPendingOp{..} _) = vpDelete
 
-senderLogic :: Env -> Send -> IO FromX -> IO ()
+senderLogic :: Env -> (BS -> Peer -> IO ()) -> IO FromX -> IO ()
 senderLogic env send fromX =
     handledLoop env "senderUDP" $ senderLogic' send fromX
 
-senderLogic' :: Send -> IO FromX -> IO ()
+senderLogic' :: (BS -> Peer -> IO ()) -> IO FromX -> IO ()
 senderLogic' send fromX = do
     Output bs _ peerInfo <- fromX
     send bs peerInfo
