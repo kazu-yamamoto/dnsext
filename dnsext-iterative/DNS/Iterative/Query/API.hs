ce71de9c153afc5e18089d4c169c14a78b768081
Kei Hibino <ex8k.hibino@gmail.com>
2025-03-31 17:58:29 +0900

    dnsext-iterative: query: monad-query: dep8: API

--- dnsext-iterative/DNS/Iterative/Query/API.hs
+++ dnsext-iterative/DNS/Iterative/Query/API.hs
@@ -52,7 +52,7 @@ foldResponseIterative'
 foldResponseIterative' deny reply env@Env{..} ident qs q =
     queryControls' $ \fl eh -> foldResponse' "resp-queried'" deny reply env ident qs q fl eh (resolveStub reply nsid_ ident qs eh)
 
-resolveStub :: (VResult -> DNSMessage -> a) -> Maybe OD_NSID -> Identifier -> [Question] -> EDNSheader -> DNSQuery a
+resolveStub :: MonadQuery m => (VResult -> DNSMessage -> a) -> Maybe OD_NSID -> Identifier -> [Question] -> EDNSheader -> m a
 resolveStub reply nsid ident qs eh = do
     ((cnrrs, _rn), etm) <- resolve =<< asksQP origQuestion_
     reqDO <- asksQP requestDO_
@@ -110,13 +110,13 @@ foldResponse' name deny reply env@Env{..} ident qs q@(Question bn typ cls) reqF 
 -----
 
 {- FOURMOLU_DISABLE -}
-logQueryErrors :: String -> DNSQuery a -> DNSQuery a
+logQueryErrors :: MonadQuery m => String -> m a -> m a
 logQueryErrors prefix q = do
-      handleQueryError left return q
+      q `catchQuery` left
     where
       left qe = do
           logQueryError qe
-          throwError qe
+          throwQuery qe
       logQueryError qe = case qe of
           DnsError de ss           -> logDnsError de ss
           ExtraError ee addrs msg  -> extraError logNotResponse logInvalidEDNS logRcodeError logBogus ee addrs msg
