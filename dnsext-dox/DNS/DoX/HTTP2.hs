d37ecd65aaa7512207af087d25032c7210390cfa
Kazu Yamamoto <kazu@iij.ad.jp>
2025-05-01 14:07:56 +0900

    setting :authority and SNI to http2-tls

--- dnsext-dox/DNS/DoX/HTTP2.hs
+++ dnsext-dox/DNS/DoX/HTTP2.hs
@@ -28,6 +28,7 @@ import Data.ByteString.Short (fromShort)
 import qualified Data.ByteString.Short as Short
 import Network.HTTP.Types
 import Network.HTTP2.Client (Client, Response, SendRequest, getResponseBodyChunk, requestBuilder, responseStatus)
+import qualified Network.HTTP2.Client as H2
 import qualified Network.HTTP2.TLS.Client as H2TLS
 import System.Timeout (timeout)
 
@@ -43,37 +44,52 @@ withTimeout ResolveInfo{..} action = do
 
 http2PersistentResolver :: PersistentResolver
 http2PersistentResolver ri@ResolveInfo{..} body = do
+    -- TLS SNI
     settings <- makeSettings ri tag
     ident <- ractionGenId rinfoActions
-    H2TLS.run settings (show rinfoIP) rinfoPort $
+    H2TLS.runWithConfig config settings ipstr rinfoPort $
         doHTTP tag ident ri body
   where
     tag = nameTag ri "H2"
+    ipstr = show rinfoIP
+    -- HTTP :authority
+    config = H2.defaultClientConfig{H2.authority = fromMaybe ipstr rinfoServerName}
 
 http2Resolver :: OneshotResolver
 http2Resolver ri@ResolveInfo{..} q qctl = do
     settings <- makeSettings ri tag
     ident <- ractionGenId rinfoActions
     withTimeout ri $
-        H2TLS.run settings (show rinfoIP) rinfoPort $
+        H2TLS.runWithConfig config settings ipstr rinfoPort $
             doHTTPOneshot tag ident ri q qctl
   where
     tag = nameTag ri "H2"
+    ipstr = show rinfoIP
+    -- HTTP :authority
+    config = H2.defaultClientConfig{H2.authority = fromMaybe ipstr rinfoServerName}
 
 http2cPersistentResolver :: PersistentResolver
 http2cPersistentResolver ri@ResolveInfo{..} body = do
     let tag = nameTag ri "H2C"
     ident <- ractionGenId rinfoActions
-    H2TLS.runH2C H2TLS.defaultSettings (show rinfoIP) rinfoPort $
+    H2TLS.runH2CWithConfig config H2TLS.defaultSettings ipstr rinfoPort $
         doHTTP tag ident ri body
+  where
+    ipstr = show rinfoIP
+    -- HTTP :authority
+    config = H2.defaultClientConfig{H2.authority = fromMaybe ipstr rinfoServerName}
 
 http2cResolver :: OneshotResolver
 http2cResolver ri@ResolveInfo{..} q qctl = do
     let tag = nameTag ri "H2C"
     ident <- ractionGenId rinfoActions
     withTimeout ri $
-        H2TLS.runH2C H2TLS.defaultSettings (show rinfoIP) rinfoPort $
+        H2TLS.runH2CWithConfig config H2TLS.defaultSettings ipstr rinfoPort $
             doHTTPOneshot tag ident ri q qctl
+  where
+    ipstr = show rinfoIP
+    -- HTTP :authority
+    config = H2.defaultClientConfig{H2.authority = fromMaybe ipstr rinfoServerName}
 
 resolv
     :: NameTag
