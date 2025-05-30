74304905f115d2bd787d06f31ec0c6461a9b8ed9
Kei Hibino <ex8k.hibino@gmail.com>
2025-05-30 21:54:43 +0900

    dnsext-utils: explicitly apply IO dcon

--- dnsext-utils/DNS/Array.hs
+++ dnsext-utils/DNS/Array.hs
@@ -6,10 +6,10 @@
 
 module DNS.Array where
 
-import Control.Monad.Primitive (primitive)
 import Data.Array.Base (STUArray (..))
 import Data.Array.IO.Internals (IOUArray (..))
 import Data.Array.MArray (Ix (..))
+import GHC.IO (IO (..))
 import GHC.Exts (Int (..), atomicReadIntArray#, casIntArray#, (==#))
 
 atomicModifyIntArray :: Ix ix => IOUArray ix Int -> ix -> (Int -> Int) -> IO Int
@@ -25,7 +25,7 @@ atomicModifyIntArray (IOUArray (STUArray l u _s mba)) ix f =
                             case o# ==# o'# of
                                 0# -> go s# o'#
                                 _ -> (# s'#, I# o# #)
-         in primitive $ \s# ->
+         in IO $ \s# ->
                 case atomicReadIntArray# mba# i# s# of
                     (# s'#, o# #) -> go s'# o#
     {-# INLINE atomicModify #-}
