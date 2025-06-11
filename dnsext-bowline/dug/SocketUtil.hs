acf3e60a35f6c6500d570576db95cdc03c4efe9e
Kazu Yamamoto <kazu@iij.ad.jp>
2025-06-11 15:39:25 +0900

    refactoring SocketUtil

--- dnsext-bowline/dug/SocketUtil.hs
+++ dnsext-bowline/dug/SocketUtil.hs
@@ -2,27 +2,30 @@ module SocketUtil (checkDisableV6) where
 
 import Control.Exception (bracket)
 import Data.IP (IP (..), IPv6)
+import qualified Data.List.NonEmpty as NE
 import Network.Socket (AddrInfo (..), AddrInfoFlag (..), SocketType (..))
 import qualified Network.Socket as S
 import System.IO.Error (tryIOError)
 
 checkDisableV6 :: [IP] -> IO Bool
 checkDisableV6 addrs
-    | v6 : _ <- v6s = either (disabled . show) pure =<< tryIOError (checkV6 v6)
+    | v6 : _ <- v6s = either disabled pure =<< tryIOError (checkV6 v6)
     | otherwise = pure False
   where
     v6s = [v6 | IPv6 v6 <- addrs]
-
-disabled :: String -> IO Bool
-disabled e = do
-    putStrLn ("disabling IPv6: " ++ e)
-    return True
+    disabled e = do
+        putStrLn ("disabling IPv6: " ++ show e)
+        return True
 
 checkV6 :: IPv6 -> IO Bool
 checkV6 dst = do
-    remote <- S.getAddrInfo (Just hint) (Just (show dst)) (Just "53")
-    checkRoute remote
+    ai@AddrInfo{addrAddress = peer} <-
+        NE.head <$> S.getAddrInfo (Just hint) (Just addr) (Just port)
+    bracket (S.openSocket ai) S.close $ \s -> S.connect s peer
+    return False
   where
+    addr = show dst
+    port = "53" -- "0" causes an error on BSD variants
     -- Check whether IPv6 is available by specifying `AI_ADDRCONFIG`
     -- to `addrFlags` of hints passed to `getAddrInfo`.  If `Nothing`
     -- is passed to `hints`, the default value of `addrFlags` is
@@ -39,8 +42,3 @@ checkV6 dst = do
             { addrFlags = [AI_ADDRCONFIG]
             , addrSocketType = Datagram
             }
-
-    checkRoute (ai@AddrInfo{addrAddress = peer} : _) = do
-        bracket (S.openSocket ai) S.close $ \s -> S.connect s peer
-        return False
-    checkRoute _ = disabled "cannot get IPv6 address"
