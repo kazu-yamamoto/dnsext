88ce88d403744fd02aceea25aa3eea35e384754d
Kazu Yamamoto <kazu@iij.ad.jp>
2025-06-12 13:57:35 +0900

    fixing a bug of svcbResolveInfos

--- dnsext-dox/DNS/DoX/Client.hs
+++ dnsext-dox/DNS/DoX/Client.hs
@@ -103,8 +103,9 @@ lookupSVCBInfo lenv@LookupEnv{..} = do
         Left err -> return $ Left err
         Right res -> do
             let ss = extractSVCB res
+                -- mainly to pass 'rinfoActions'
                 ri = NE.head $ renvResolveInfos lenvResolveEnv
-                addss = svcbResolveInfos ri ss
+                addss = svcbResolveInfos (replyTag res) ri ss
             logIt lenv ss
             return $ Right addss
 
@@ -140,24 +141,24 @@ toPipelineResolver SVCBInfo{..}
 
 ----------------------------------------------------------------
 
-svcbResolveInfos :: ResolveInfo -> [RD_SVCB] -> [[SVCBInfo]]
-svcbResolveInfos ri ss = onPriority ri <$> ss
+svcbResolveInfos :: NameTag -> ResolveInfo -> [RD_SVCB] -> [[SVCBInfo]]
+svcbResolveInfos ntag ri ss = onPriority ntag ri <$> ss
 
-onPriority :: ResolveInfo -> RD_SVCB -> [SVCBInfo]
-onPriority ri s = case extractSvcParam SPK_ALPN $ svcb_params s of
+onPriority :: NameTag -> ResolveInfo -> RD_SVCB -> [SVCBInfo]
+onPriority ntag ri s = case extractSvcParam SPK_ALPN $ svcb_params s of
     Nothing -> []
-    Just alpns -> onALPN ri s <$> alpn_names alpns
+    Just alpns -> onALPN ntag ri s <$> alpn_names alpns
 
-onALPN :: ResolveInfo -> RD_SVCB -> ALPN -> SVCBInfo
-onALPN ri s alpn =
+onALPN :: NameTag -> ResolveInfo -> RD_SVCB -> ALPN -> SVCBInfo
+onALPN ntag ri s alpn =
     SVCBInfo
         { svcbInfoALPN = alpn
-        , svcbInfoNameTag = nameTag ri "UDP"
-        , svcbInfoResolveInfos = extractResolveInfo ri s alpn
+        , svcbInfoNameTag = ntag
+        , svcbInfoResolveInfos = extractResolveInfo ntag ri s alpn
         }
 
-extractResolveInfo :: ResolveInfo -> RD_SVCB -> ShortByteString -> [ResolveInfo]
-extractResolveInfo ri s alpn = updateIPPort <$> ips
+extractResolveInfo :: NameTag -> ResolveInfo -> RD_SVCB -> ShortByteString -> [ResolveInfo]
+extractResolveInfo ntag ri s alpn = updateIPPort <$> ips
   where
     params = svcb_params s
     target = svcb_target s
@@ -170,6 +171,12 @@ extractResolveInfo ri s alpn = updateIPPort <$> ips
         Nothing -> []
         Just v6 -> show <$> hint_ipv6s v6
     ips = case v4s ++ v6s of
-        [] -> [(rinfoIP ri, port)] -- no "ipv4hint" nor "ipv6hint"
+        [] -> [(nameTagIP ntag, port)] -- no "ipv4hint" nor "ipv6hint"
         xs -> (\h -> (fromString h, port)) <$> xs
-    updateIPPort (x, y) = ri{rinfoIP = x, rinfoPort = y, rinfoPath = mdohpath, rinfoServerName = Just $ init $ toRepresentation target}
+    updateIPPort (x, y) =
+        ri
+            { rinfoIP = x
+            , rinfoPort = y
+            , rinfoPath = mdohpath
+            , rinfoServerName = Just $ init $ toRepresentation target
+            }
