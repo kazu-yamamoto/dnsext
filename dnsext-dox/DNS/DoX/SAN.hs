18079b89affa8d50fe4d5c463edf9fd819bda5f5
Kazu Yamamoto <kazu@iij.ad.jp>
2025-06-14 17:22:39 +0900

    improving SAN implementation

--- dnsext-dox/DNS/DoX/SAN.hs
+++ dnsext-dox/DNS/DoX/SAN.hs
@@ -1,19 +1,15 @@
 module DNS.DoX.SAN (makeOnServerCertificate) where
 
-import Data.ASN1.BinaryEncoding (DER (..))
-import Data.ASN1.BinaryEncoding.Raw (ASN1Class (..))
-import Data.ASN1.Encoding (decodeASN1)
-import Data.ASN1.Prim (ASN1 (..))
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BL
 import Data.IP (IP (..))
 import qualified Data.IP as IP
 import Data.Maybe (catMaybes)
-import Data.Word (Word8)
 import Data.X509 (
+    AltName (..),
     Certificate (..),
-    ExtensionRaw (..),
-    Extensions (..),
+    ExtSubjectAltName (..),
+    certExtensions,
+    extensionGet,
     getCertificate,
  )
 import Data.X509.Validation (FailedReason (..), validateDefault)
@@ -29,32 +25,23 @@ makeOnServerCertificate (Just ip) = f
       where
         ips = getSAN cc
 
-oidSAN :: [Integer]
-oidSAN = [2, 5, 29, 17]
-
 getSAN :: CertificateChain -> [IP]
 getSAN (CertificateChain []) = []
-getSAN (CertificateChain (cert : _)) = case sans of
-    [] -> []
-    san : _ -> case decodeSAN san of
-        Left _ -> []
-        Right asn1s -> catMaybes $ map toIPAddr $ asn1s
-  where
-    exts = case certExtensions $ getCertificate cert of
-        Extensions (Just es) -> es
-        _ -> []
-    sans = filter (\x -> extRawOID x == oidSAN) exts
-    decodeSAN = decodeASN1 DER . BL.fromStrict . extRawContent
+getSAN (CertificateChain (cert : _)) = getNames $ getCertificate cert
 
-toIPAddr :: ASN1 -> Maybe IP
-toIPAddr (Other Context 7 x) = Just $ toIP $ BS.unpack $ x
-toIPAddr _ = Nothing
-
-toIP :: [Word8] -> IP
-toIP ws
-    | len == 4 = IPv4 $ IP.toIPv4 is
-    | len == 16 = IPv6 $ IP.toIPv6b is
-    | otherwise = error "toIP"
+getNames :: Certificate -> [IP]
+getNames cert = maybe [] toAltName $ extensionGet $ certExtensions cert
   where
+    toAltName (ExtSubjectAltName names) = catMaybes $ map unAltName names
+    unAltName (AltNameIP s) = toIP s
+    unAltName _ = Nothing
+
+toIP :: BS.ByteString -> Maybe IP
+toIP bs
+    | len == 4 = Just $ IPv4 $ IP.toIPv4 is
+    | len == 16 = Just $ IPv6 $ IP.toIPv6b is
+    | otherwise = Nothing
+  where
+    ws = BS.unpack bs
     len = length ws
     is = map fromIntegral ws
