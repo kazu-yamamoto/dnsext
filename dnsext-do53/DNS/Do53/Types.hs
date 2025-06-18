827558b29aecc9d6edd7ffe90f3af64be9af00e7
Kazu Yamamoto <kazu@iij.ad.jp>
2025-06-18 14:50:42 +0900

    fixing doc

--- dnsext-do53/DNS/Do53/Types.hs
+++ dnsext-do53/DNS/Do53/Types.hs
@@ -110,11 +110,11 @@ newtype VCLimit = VCLimit {unVCLimit :: Int} deriving (Eq, Ord, Num, Show)
 --
 --  An example to use Google's public DNS cache instead of resolv.conf:
 --
---  > let conf = defaultLookupConf { lconfInfo = RCAddr "8.8.8.8" }
+--  > let conf = defaultLookupConf { lconfInfo = SeedsAddr "8.8.8.8" }
 --
 --  An example to use multiple Google's public DNS cache concurrently:
 --
---  > let conf = defaultLookupConf { lconfInfo = RCAddrs ["8.8.8.8","8.8.4.4"], lconfConcurrent = True }
+--  > let conf = defaultLookupConf { lconfInfo = SeedsAddrs ["8.8.8.8","8.8.4.4"], lconfConcurrent = True }
 --
 --  An example to disable EDNS:
 --
